535a68d911aa3e9977bba6ebb0498be5d1cd536d
Laurent Montel <montel@kde.org>
2009-06-08 11:45:51 +0000

    Fix enable/disable
    svn path=/trunk/playground/office/kmymoney/; revision=978864

--- kmymoney2/views/kmymoneyview.cpp
+++ kmymoney2/views/kmymoneyview.cpp
@@ -476,11 +476,9 @@ void KMyMoneyView::enableViews(int state)
   m_institutionsViewFrame->setEnabled(state);
   m_scheduleViewFrame->setEnabled(state);
   m_categoriesViewFrame->setEnabled(state);
-#warning "port to kde4"
-
   m_payeesViewFrame->setEnabled(state);
   m_budgetViewFrame->setEnabled(state);
-  //m_ledgerViewFrame->setEnabled(state);
+  m_ledgerViewFrame->setEnabled(state);
   m_investmentViewFrame->setEnabled(state);
   m_reportsViewFrame->setEnabled(state);
   m_forecastViewFrame->setEnabled(state);
