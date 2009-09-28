6e09c17a7a716e0cc10ead86b2b38de5fffcf3f9
Cristian Oneț <onet.cristian@gmail.com>
2009-09-28 03:25:20 +0000

    Fixed the licenses.
    svn path=/trunk/playground/office/kmymoney/; revision=1028744

--- kmymoney2/plugins/icalendarexport/kcm_icalendarexport.cpp
+++ kmymoney2/plugins/icalendarexport/kcm_icalendarexport.cpp
@@ -1,11 +1,13 @@
 /***************************************************************************
- *   Copyright (C) 2008 by Cristian Onet                                   *
- *   onet.cristian@gmail.com                                               *
+ *   Copyright 2009  Cristian Onet onet.cristian@gmail.com                 *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   This program is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU General Public License as        *
+ *   published by the Free Software Foundation; either version 2 of        *
+ *   the License or (at your option) version 3 or any later version        *
+ *   accepted by the membership of KDE e.V. (or its successor approved     *
+ *   by the membership of KDE e.V.), which shall act as a proxy            *
+ *   defined in Section 14 of version 3 of the license.                    *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
@@ -13,10 +15,9 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
  ***************************************************************************/
+
 // Qt includes
 #include <qcombobox.h>
 
