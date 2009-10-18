5c6b174a3d51f3ddbb52273246bf5e2d8db9a3cd
Cristian Oneț <onet.cristian@gmail.com>
2009-10-18 17:05:43 +0000

    Re-add the create links action because without it kmymoney can't be build on a clean system (a system that does not have kmymoney headers in the include path).
    svn path=/trunk/playground/office/kmymoney/; revision=1037238

---
+++ cmake/modules/KMyMoneyMacros.cmake
@@ -0,0 +1,29 @@
+#
+# this file contains the following macros:
+# KMM_CREATE_LINKS
+# KMM_CREATE_LINKS_BIN
+
+#############################################################################
+# Create Links
+#############################################################################
+
+IF (NOT EXISTS ${KMyMoney_BINARY_DIR}/kmymoney)
+  FILE(MAKE_DIRECTORY ${KMyMoney_BINARY_DIR}/kmymoney)
+ENDIF (NOT EXISTS ${KMyMoney_BINARY_DIR}/kmymoney)
+
+MACRO(KMM_CREATE_LINKS)
+  FOREACH(c_FILE ${ARGV})
+    EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E create_symlink
+      ${CMAKE_CURRENT_SOURCE_DIR}/${c_FILE}
+      ${KMyMoney_BINARY_DIR}/kmymoney/${c_FILE})
+  ENDFOREACH (c_FILE)
+ENDMACRO(KMM_CREATE_LINKS)
+
+MACRO(KMM_CREATE_LINKS_BIN)
+  FOREACH(c_FILE ${ARGV})
+    EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -E create_symlink
+      ${CMAKE_CURRENT_BINARY_DIR}/${c_FILE}
+      ${KMyMoney_BINARY_DIR}/kmymoney/${c_FILE})
+  ENDFOREACH (c_FILE)
+ENDMACRO(KMM_CREATE_LINKS_BIN)
+
