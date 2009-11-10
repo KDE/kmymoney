a866782299fd2e3f0a76d65db0e712b3cbbf9766
Alvaro Soliverez <asoliverez@kde.org>
2009-11-10 01:10:02 +0000

    Updated po name following the application name change
    svn path=/trunk/playground/office/kmymoney/; revision=1046953

--- Messages.sh
+++ Messages.sh
@@ -7,7 +7,7 @@ cd kmymoney2/ && $PREPARETIPS > tips.cpp
 cd ..
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kmymoney2.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kmymoney.pot
 
 # Remove these two generated files again
 rm kmymoney2/tips.cpp rc.cpp
