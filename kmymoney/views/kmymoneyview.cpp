b179f6d96a1f44f49ee97571835ac55aaa5a2a2b
Alvaro Soliverez <asoliverez@kde.org>
2009-11-28 22:10:17 +0000

    Added the whole string into the i18n call. It doesn't make sense to have an additional QString just for a tag.
    svn path=/trunk/playground/office/kmymoney/; revision=1055868

--- kmymoney/views/kmymoneyview.cpp
+++ kmymoney/views/kmymoneyview.cpp
@@ -983,7 +983,7 @@ void KMyMoneyView::saveToLocalFile(const QString& localFile, IMyMoneyStorageForm
     if(KMyMoneyGlobalSettings::encryptRecover()) {
       encryptRecover = true;
       if(!KGPGFile::keyAvailable(QString(RECOVER_KEY_ID))) {
-        KMessageBox::sorry(this, QString("<p>")+i18n("You have selected to encrypt your data also with the KMyMoney recover key, but the key with id</p><p><center><b>%1</b></center></p><p>has not been found in your keyring at this time. Please make sure to import this key into your keyring. You can find it on the <a href=\"http://kmymoney2.sourceforge.net/\">KMyMoney web-site</a>. This time your data will not be encrypted with the KMyMoney recover key.</p>",QString( RECOVER_KEY_ID) ), i18n("GPG-Key not found"));
+        KMessageBox::sorry(this, i18n("<p>You have selected to encrypt your data also with the KMyMoney recover key, but the key with id</p><p><center><b>%1</b></center></p><p>has not been found in your keyring at this time. Please make sure to import this key into your keyring. You can find it on the <a href=\"http://kmymoney2.sourceforge.net/\">KMyMoney web-site</a>. This time your data will not be encrypted with the KMyMoney recover key.</p>",QString( RECOVER_KEY_ID) ), i18n("GPG-Key not found"));
         encryptRecover = false;
       }
     }
