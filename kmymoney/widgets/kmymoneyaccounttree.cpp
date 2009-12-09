1acff8a29d3a8d8a9238be87104d02ab6284db0d
Cristian Oneț <onet.cristian@gmail.com>
2009-12-09 19:17:00 +0000

    BUG: 217905
    Fixed missing icon files in accounts and categories view. Replaced the "ok" icon with the "dialog-ok" icon.
    
    svn path=/trunk/playground/office/kmymoney/; revision=1060725

--- kmymoney/widgets/kmymoneyaccounttree.cpp
+++ kmymoney/widgets/kmymoneyaccounttree.cpp
@@ -90,7 +90,7 @@ void KMyMoneyAccountTreeItem::fillColumns()
   if (!lv)
     return;
   KMyMoneyAccountTreeBaseItem::fillColumns();
-  QPixmap checkMark = QPixmap(KIconLoader::global()->loadIcon("ok", KIconLoader::Small));
+  QPixmap checkMark = QPixmap(KIconLoader::global()->loadIcon("dialog-ok", KIconLoader::Small));
   MyMoneyMoney vatRate;
   if (!isInstitution())
     setPixmap(lv->nameColumn(), m_account.accountPixmap(m_reconcileFlag, 22));
