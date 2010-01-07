89a74f4f6a8d8aa6e42b7ab70c7ef19a8b177667
Alvaro Soliverez <asoliverez@kde.org>
2010-01-07 18:23:50 +0000

    Updated welcome page's version number and added some items to the features list
    svn path=/trunk/playground/office/kmymoney/; revision=1071207

--- kmymoney/views/kwelcomepage.cpp
+++ kmymoney/views/kwelcomepage.cpp
@@ -152,7 +152,7 @@ const QString KWelcomePage::whatsNewPage(void)
   body += QString("<img id=\"background_image\" src=\"%1\" height=\"100%\">").arg(backgroundFilename);
   const QString logoFilename = KGlobal::dirs()->findResource("appdata", "html/images/trans_logo.png");
   body += QString("<img id=\"KMyMoneyLogo\" src=\"%1\">").arg(logoFilename);
-  body += QString("<h3 id=\"title\">" + i18n("What's new in KMyMoney 1.0") +"</h3>");
+  body += QString("<h3 id=\"title\">" + i18n("What's new in KMyMoney 3.96") +"</h3>");
   const QString backArrowFilename = KGlobal::dirs()->findResource("appdata", "html/images/backarrow.png");
   body += QString("<div id=\"returnLink\"><img src=\"%1\">").arg(backArrowFilename);
   body += QString("<a href=\"/welcome\">" + i18n("Return to the Welcome page") + "</a></div>");
@@ -206,6 +206,8 @@ const QStringList KWelcomePage::featuresList(void)
 
   featuresList.append(i18n("Port to KDE4"));
   featuresList.append(i18n("Documentation and translations have been improved"));
+  featuresList.append(i18n("Now works with latest version of AqBanking"));
+  featuresList.append(i18n("Runs on all operating systems supported by KDE"));
 
   return featuresList;
 }
