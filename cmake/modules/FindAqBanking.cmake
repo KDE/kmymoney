6c09e590912e0eef65abe6a00c2b5998b8ce3418
Thomas Baumgart <thb@net-bembel.de>
2010-01-08 16:50:03 +0000

    Improve detection of correct AqBanking version
    svn path=/trunk/playground/office/kmymoney/; revision=1071746

--- cmake/modules/FindAqBanking.cmake
+++ cmake/modules/FindAqBanking.cmake
@@ -50,35 +50,32 @@ set(AQBANKING_INCLUDE_DIRS ${AQBANKING_INCLUDE_DIR})
 set(AQBANKING_LIBRARIES ${AQBANKING_LIBRARY} ${QBANKING_LIBRARY})
 
 if(AQBANKING_INCLUDE_DIRS AND AQBANKING_LIBRARIES)
-  set(FIND_AQBANKING_VERSION_SOURCE
-    "#include <stdio.h>\n #include <aqbanking/version.h>\n int main()\n {\n printf(\"%s\",AQBANKING_VERSION_STRING);return 1;\n }\n")
-  set(FIND_AQBANKING_VERSION_SOURCE_FILE ${CMAKE_BINARY_DIR}/CMakeTmp/FindAQBANKING.cxx)
-  file(WRITE "${FIND_AQBANKING_VERSION_SOURCE_FILE}" "${FIND_AQBANKING_VERSION_SOURCE}")
-
-  set(FIND_AQBANKING_VERSION_ADD_INCLUDES
-    "-DINCLUDE_DIRECTORIES:STRING=${AQBANKING_INCLUDE_DIRS}")
-
-  try_run(RUN_RESULT COMPILE_RESULT
-    ${CMAKE_BINARY_DIR}
-    ${FIND_AQBANKING_VERSION_SOURCE_FILE}
-    CMAKE_FLAGS "${FIND_AQBANKING_VERSION_ADD_INCLUDES}"
-    RUN_OUTPUT_VARIABLE AQBANKING_VERSION)
-
-  if(COMPILE_RESULT AND RUN_RESULT EQUAL 1)
-    message(STATUS "Found AqBanking version ${AQBANKING_VERSION}")
+  execute_process(COMMAND ${AQBASEDIR}/bin/aqbanking-config --vstring OUTPUT_VARIABLE AQBANKING_VERSION RESULT_VARIABLE RUN_RESULT)
+  if(RUN_RESULT EQUAL 0)
+    # get rid of trailing \n
+    string(LENGTH ${AQBANKING_VERSION} LEN)
+    math(EXPR LEN ${LEN}-1)
+    string(SUBSTRING ${AQBANKING_VERSION} 0 ${LEN} AQBANKING_VERSION)
+    message(STATUS "Found AqBanking version '${AQBANKING_VERSION}' in ${AQBASEDIR}")
     macro_ensure_version2(${AQBANKING_MIN_VERSION} ${AQBANKING_VERSION} AQBANKING_VERSION_OK)
     if(NOT AQBANKING_VERSION_OK)
       message(STATUS "AqBanking version ${AQBANKING_VERSION} is too old. At least version ${AQBANKING_MIN_VERSION} is needed.")
       set(AQBANKING_INCLUDE_DIRS "")
       set(AQBANKING_LIBRARIES "")
     else(NOT AQBANKING_VERSION_OK)
-      set(AQBANKING_FOUND TRUE)
+      execute_process(COMMAND grep qt3to4 ${AQBANKING_INCLUDE_DIRS}/qbanking/qbanking.h RESULT_VARIABLE RUN_RESULT OUTPUT_QUIET)
+      if(RUN_RESULT EQUAL 0)
+        set(AQBANKING_FOUND TRUE)
+      else(RUN_RESULT EQUAL 0)
+        message(STATUS "The AqBanking version found is for usage with Qt3 - we don't use it")
+      endif(RUN_RESULT EQUAL 0)
     endif(NOT AQBANKING_VERSION_OK)
-  else(COMPILE_RESULT AND RUN_RESULT EQUAL 1)
-    message(FATAL_ERROR "Unable to compile or run the AqBanking version detection program.")
-  endif(COMPILE_RESULT AND RUN_RESULT EQUAL 1)
+  endif(RUN_RESULT EQUAL 0)
 else(AQBANKING_INCLUDE_DIRS AND AQBANKING_LIBRARIES)
-  message(STATUS "Could not find AqBanking headers and the KBanking plugin will not be compiled. If you do have the AqBanking development package installed use the AQBANKING_BASE cmake variable to point to the location where AqBanking is installed")
+  message(STATUS "AqBanking headers not found. If you do have")
+  message(STATUS "  the AqBanking development package installed use")
+  message(STATUS "  the AQBANKING_BASE cmake variable to point to")
+  message(STATUS "  the location where AqBanking is installed.")
 endif(AQBANKING_INCLUDE_DIRS AND AQBANKING_LIBRARIES)
 
 mark_as_advanced(AQBANKING_INCLUDE_DIRS AQBANKING_LIBRARIES)
