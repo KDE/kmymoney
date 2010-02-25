d9187a901e789fc056caca0e6afdba928185960d
Thomas Baumgart <thb@net-bembel.de>
2010-02-25 08:50:59 +0000

    Support new option tags
    svn path=/trunk/playground/office/kmymoney/; revision=1095919

--- astyle-kmymoney.sh
+++ astyle-kmymoney.sh
@@ -10,8 +10,22 @@
 IFS=:
 for path in $PATH; do
   if test -x $path/astyle; then
+    # determine version of AStyle
+    VER=`$path/astyle --version 2>&1 | cut -d' ' -f4`
+
+    # there was an option change between 1.23 and 1.24
+    # we default to the newer version here as this is the future
+    case $VER in
+      1.2[1-3])
+        OPT="--pad=oper --unpad=paren"
+        ;;
+      *)
+        OPT="--pad-oper --unpad-paren"
+        ;;
+    esac
+
     $path/astyle --indent=spaces=2 --brackets=linux \
-      --indent-labels --pad=oper --unpad=paren \
+      --indent-labels ${OPT} \
       --one-line=keep-statements --convert-tabs \
       --indent-preprocessor \
       `find kmymoney libkgpgfile -type f  \( -name \*.c -or -name \*.cpp -or -name \*.h \)  `
