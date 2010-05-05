8e516c195f9eb36cab7a18fce8963ad09de6f072
Cristian Oneț <onet.cristian@gmail.com>
2010-05-05 18:24:05 +0000

    Allow using the context menu inside the widget.
    svn path=/trunk/playground/office/kmymoney/; revision=1123261

--- kmymoney/widgets/kmymoneymvccombo.cpp
+++ kmymoney/widgets/kmymoneymvccombo.cpp
@@ -205,6 +205,12 @@ void KMyMoneyMVCCombo::disconnectNotify(const char* signal)
 
 void KMyMoneyMVCCombo::focusOutEvent(QFocusEvent* e)
 {
+  // when showing m_completion we'll receive a focus out event even if the focus
+  // will still remain at this widget since this widget is the completion's focus proxy
+  // so ignore the focus out event caused by showin a widget of type Qt::Popup
+  if (e->reason() == Qt::PopupFocusReason)
+    return;
+
   if (d->m_inFocusOutEvent) {
     KComboBox::focusOutEvent(e);
     return;
