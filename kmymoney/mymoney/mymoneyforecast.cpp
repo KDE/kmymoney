e93804312dce14494a855b8e88bf74d7503a821a
Cristian Oneț <onet.cristian@gmail.com>
2010-05-13 05:00:09 +0000

    Run astyle-kmymoney.sh
    svn path=/trunk/playground/office/kmymoney/; revision=1126065

--- kmymoney/mymoney/mymoneyforecast.cpp
+++ kmymoney/mymoney/mymoneyforecast.cpp
@@ -844,20 +844,18 @@ MyMoneyMoney MyMoneyForecast::accountCycleVariation(const MyMoneyAccount& acc)
   MyMoneyMoney cycleVariation;
 
   if (forecastMethod() == eHistoric) {
-    switch(historyMethod()) {
-      case 0:
-      case 1:
-      {
-        for (int t_day = 1; t_day <= accountsCycle() ; ++t_day) {
-          cycleVariation += m_accountTrendList[acc.id()][t_day];
-        }
+    switch (historyMethod()) {
+    case 0:
+    case 1: {
+      for (int t_day = 1; t_day <= accountsCycle() ; ++t_day) {
+        cycleVariation += m_accountTrendList[acc.id()][t_day];
       }
+    }
+    break;
+    case 2: {
+      cycleVariation = m_accountList[acc.id()][QDate::currentDate().addDays(accountsCycle())] - m_accountList[acc.id()][QDate::currentDate()];
       break;
-      case 2:
-      {
-        cycleVariation = m_accountList[acc.id()][QDate::currentDate().addDays(accountsCycle())] - m_accountList[acc.id()][QDate::currentDate()];
-        break;
-      }
+    }
     }
   }
   return cycleVariation;
