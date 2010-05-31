22022cf5201c0c3149c2471f7da966432b456d0d
Andrew Coles <andrew.i.coles@googlemail.com>
2010-05-31 16:59:15 +0000

    Minor proof-reading changes: - in pixel -> in pixels - Changed warning to idiomatic English (you have the understanding that -> you understand that)
    svn path=/trunk/kdereview/kmymoney/; revision=1132892

--- kmymoney/views/kmymoneyview.cpp
+++ kmymoney/views/kmymoneyview.cpp
@@ -1047,7 +1047,7 @@ void KMyMoneyView::saveToLocalFile(const QString& localFile, IMyMoneyStorageForm
     }
 
     if (encryptedOk == true) {
-      QString msg = i18n("<p>You have configured to save your data in encrypted form using GPG. Make sure, you have the necessary understanding that you might loose all your data if you store it encrypted and cannot decrypt it later on. If unsure, answer <b>No</b>.</p>");
+      QString msg = i18n("<p>You have configured to save your data in encrypted form using GPG. Make sure you understand that you might lose all your data if you encrypt it, but cannot decrypt it later on. If unsure, answer <b>No</b>.</p>");
 
       if (KMessageBox::questionYesNo(this, msg, i18n("Store GPG encrypted"), KStandardGuiItem::yes(), KStandardGuiItem::no(), "StoreEncrypted") == KMessageBox::No) {
         encryptedOk = false;
