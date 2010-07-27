52cf3860c073fb16df498a189868c9c7acafe054
Thomas Baumgart <thb@net-bembel.de>
2010-07-27 08:49:30 +0000

    Make sure we have an AqBanking version that is know to work with KBanking. Newer version than 4.99.8 are currently not supported.
    svn path=/trunk/extragear/office/kmymoney/; revision=1155311

--- cmake/modules/FindAqBanking.cmake
+++ cmake/modules/FindAqBanking.cmake
@@ -11,12 +11,19 @@ if(NOT AQBANKING_MIN_VERSION)
   set(AQBANKING_MIN_VERSION "4.2.4")
 endif(NOT AQBANKING_MIN_VERSION)
 
+if(NOT AQBANKING_MAX_VERSION)
+  # for some unknown reason, we need to give a micro version number
+  # with an offset of 1 to the PKG_CHECK_MODULES macro. The actual
+  # version KBanking will work with is 4.99.8
+  set(AQBANKING_MAX_VERSION "4.99.9")
+endif(NOT AQBANKING_MAX_VERSION)
+
 if(AQBANKING_INCLUDE_DIRS AND AQBANKING_LIBRARIES)
   # Already in cache, be silent
   set(AQBANKING_FIND_QUIETLY TRUE)
 endif(AQBANKING_INCLUDE_DIRS AND AQBANKING_LIBRARIES)
 
-PKG_CHECK_MODULES(AQBANKING aqbanking>=${AQBANKING_MIN_VERSION})
+PKG_CHECK_MODULES(AQBANKING aqbanking>=${AQBANKING_MIN_VERSION} aqbanking<=${AQBANKING_MAX_VERSION})
 
 if(${AQBANKING_FOUND})
   # if AqBanking has been found make sure to add the q4banking lib
