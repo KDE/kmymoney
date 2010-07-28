f95db61d8d8d2aeb7849d9cdff1569aa071d819e
Alvaro Soliverez <asoliverez@kde.org>
2010-07-28 00:12:51 +0000

    Fixed the type of investment combo to not overwrite the first item with the current type.
    Moved strings around, but there are no new or modified strings.
    
    BUG:245964
    CCMAIL:kde-i18n-doc@kde.org
    
    svn path=/trunk/extragear/office/kmymoney/; revision=1155738

--- kmymoney/wizards/newinvestmentwizard/kinvestmenttypewizardpage.cpp
+++ kmymoney/wizards/newinvestmentwizard/kinvestmenttypewizardpage.cpp
@@ -19,7 +19,7 @@
 
 // ----------------------------------------------------------------------------
 // QT Includes
-
+#include <QStringListModel>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -31,11 +31,24 @@
 KInvestmentTypeWizardPage::KInvestmentTypeWizardPage(QWidget *parent)
     : KInvestmentTypeWizardPageDecl(parent)
 {
+  QStringListModel *model = new QStringListModel();
+  QStringList types;
+  types << i18n("Stock") << i18n("Mutual Fund") << i18n("Bond");
+  model->setStringList(types);
+  model->sort(0, Qt::AscendingOrder);
+
+  m_securityType->setModel(model);
 }
 
 void KInvestmentTypeWizardPage::init2(const MyMoneySecurity& security)
 {
-  m_securityType->setItemText(m_securityType->currentIndex(), KMyMoneyUtils::securityTypeToString(security.securityType()));
+  //get the current text of the security and set the combo index accordingly
+  QString text = KMyMoneyUtils::securityTypeToString(security.securityType());
+  for(int i = 0; i < m_securityType->count(); ++i) {
+    if(m_securityType->itemText(i) == text)
+      m_securityType->setCurrentIndex(i);
+  }
+
   // Register the fields with the QWizard
   registerField("securityType", m_securityType, "currentText", SIGNAL(currentIndexChanged(const QString&)));
 }
