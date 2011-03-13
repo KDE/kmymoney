a6f00fa6b1acdcb13a685849887a69575e77e827
Cristian Oneț <onet.cristian@gmail.com>
2011-03-13 22:30:29 +0000

    Since only one version of astyle is used at a time make that version explicit (for now 1.23).
    svn path=/trunk/extragear/office/kmymoney/; revision=1224738

--- astyle-kmymoney.sh
+++ astyle-kmymoney.sh
@@ -7,31 +7,24 @@
 # 
 # requirements: installed astyle 
 
+ASTYLE_VERSION=1.23
+
 IFS=:
 for path in $PATH; do
   if test -x $path/astyle; then
     # determine version of AStyle
     VER=`$path/astyle --version 2>&1 | cut -d' ' -f4`
 
-    # there was an option change between 1.22 and 1.23
-    # we default to the newer version here as this is the future
-    case $VER in
-      1.2[1-2])
-        PAD="pad=oper"
-        UNPAD="unpad=paren"
-        ONELINE="one-line=keep-statements"
-        ;;
-      *)
-        PAD="pad-oper"
-        UNPAD="unpad-paren"
-        ONELINE="keep-one-line-statements"
-        ;;
-    esac
+    # check if the needed astyle version is installed
+    if [ "$VER" != "$ASTYLE_VERSION" ]; then
+      echo "You have astyle version $VER installed. The required astyle version is $ASTYLE_VERSION, please install the required version to use this script."
+      exit
+    fi
 
     # run astyle with options on the set of files
     find kmymoney libkgpgfile -type f  \( -name \*.c -or -name \*.cpp -or -name \*.h \) -exec $path/astyle --indent=spaces=2 --brackets=linux \
-      --indent-labels --${PAD} --${UNPAD} \
-      --${ONELINE} --convert-tabs \
+      --indent-labels --pad-oper --unpad-paren \
+      --keep-one-line-statements --convert-tabs \
       --indent-switches --indent-cases \
       --indent-preprocessor {} \;
 
