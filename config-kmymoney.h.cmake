a8e5ae3c01028b05cfc136135e81a7ad93c342bf
Pino Toscano <pino@kde.org>
2013-05-20 17:18:19 +0200

    remove unused type sizes checks

--- config-kmymoney.h.cmake
+++ config-kmymoney.h.cmake
@@ -18,23 +18,8 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #cmakedefine HAVE_UNISTD_H 1
 
-/* The size of `char *', as computed by sizeof. */
-#cmakedefine SIZEOF_CHAR_P @SIZEOF_CHAR_P@
-
-/* The size of `int', as computed by sizeof. */
-#cmakedefine SIZEOF_INT @SIZEOF_INT@
-
 /* The size of `long', as computed by sizeof. */
 #cmakedefine SIZEOF_LONG @SIZEOF_LONG@
-
-/* The size of `short', as computed by sizeof. */
-#cmakedefine SIZEOF_SHORT @SIZEOF_SHORT@
-
-/* The size of `size_t', as computed by sizeof. */
-#cmakedefine SIZEOF_SIZE_T @SIZEOF_SIZE_T@
-
-/* The size of `unsigned long', as computed by sizeof. */
-#cmakedefine SIZEOF_UNSIGNED_LONG @SIZEOF_UNSIGNED_LONG@
 
 #cmakedefine KMM_DEBUG 1
 
