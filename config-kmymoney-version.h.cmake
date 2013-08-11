568bd77268159906d1d7852f234879da1983f9dc
Yury G. Kudryashov <urkud.urkud@gmail.com>
2013-08-11 15:18:13 +0300

    Drop unused #define's
    REVIEW: 112028

--- config-kmymoney-version.h.cmake
+++ config-kmymoney-version.h.cmake
@@ -5,10 +5,3 @@
 
 /* Version number of package */
 #define VERSION "@VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@@VERSION_SUFFIX@"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION PACKAGE_NAME " " PACKAGE_VERSION
-
