2e7d6b79c22357ad1b32a87652db52491d766fc4
Pino Toscano <pino@kde.org>
2013-09-25 12:51:39 +0200

    i18n: correctly pass the argument to i18n()

--- kmymoney/converter/mymoneystatementreader.cpp
+++ kmymoney/converter/mymoneystatementreader.cpp
@@ -931,7 +931,7 @@ void MyMoneyStatementReader::processTransactionEntry(const MyMoneyStatement::Tra
           QVBoxLayout *topcontents = new QVBoxLayout(mainWidget);
 
           //add in caption? and account combo here
-          QLabel *label1 = new QLabel(i18n("Please select a default category for payee '%1'").arg(payeename));
+          QLabel *label1 = new QLabel(i18n("Please select a default category for payee '%1'", payeename));
           topcontents->addWidget(label1);
 
           AccountNamesFilterProxyModel *filterProxyModel = new AccountNamesFilterProxyModel(this);
