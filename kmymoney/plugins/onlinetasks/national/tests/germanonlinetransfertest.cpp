e20db7e10120d8699224ea74513485258449cd86
Christian Dávid <christian-david@web.de>
2014-02-15 16:40:05 +0100

    Corrected licences
    Removed accidentally created files.

--- kmymoney/plugins/onlinetasks/national/tests/germanonlinetransfertest.cpp
+++ kmymoney/plugins/onlinetasks/national/tests/germanonlinetransfertest.cpp
@@ -1,21 +1,19 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2014  Christian David <c.david@christian-david.de>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
+ * This file is part of KMyMoney, A Personal Finance Manager for KDE
+ * Copyright (C) 2013 Christian Dávid <christian-david@web.de>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QtTest/QTest>
