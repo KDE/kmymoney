8b5707de695d470cf0986197bd94f6ea00a522c8
Christian Dávid <christian-david@web.de>
2015-01-10 18:55:12 +0100

    Corrected licence and copyright in header
    The content is still the same. Made this because ebn detected issues
    here.

--- kmymoney/kmymoneyutilstest.h
+++ kmymoney/kmymoneyutilstest.h
@@ -1,21 +1,21 @@
-/***************************************************************************
- *   Copyright 2012  Thomas Baumgart  ipwizard@users.sourceforge.net       *
- *                                                                         *
- *   This file is part of KMyMoney.                                        *
- *                                                                         *
- *   KMyMoney is free software; you can redistribute it and/or             *
- *   modify it under the terms of the GNU General Public License           *
- *   as published by the Free Software Foundation; either version 2 of     *
- *   the License or (at your option) version 3 or any later version.       *
- *                                                                         *
- *   KMyMoney is distributed in the hope that it will be useful,           *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
- ***************************************************************************/
+/*
+ * This file is part of KMyMoney, A Personal Finance Manager for KDE
+ * Copyright (C) 2012 Thomas Baumgart <ipwizard@users.sourceforge.net>
+ * Copyright (C) 2015 Christian Dávid <christian-david@web.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef KMYMONEYUTILSTEST_H
 #define KMYMONEYUTILSTEST_H
--- kmymoney/mymoney/storage/mymoneystoragesql.h
+++ kmymoney/mymoney/storage/mymoneystoragesql.h
@@ -1,21 +1,22 @@
-/***************************************************************************
-                          mymoneystoragesql.h
-                          -------------------
-    begin                : 11 November 2005
-    copyright            : (C) 2005 by Tony Bloomfield
-                         : (C) 2014 by Christian David
-    email                : tonybloom@users.sourceforge.net
-                         : Fernando Vilas <fvilas@iname.com>
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * This file is part of KMyMoney, A Personal Finance Manager for KDE
+ * Copyright (C) 2005 Tony Bloomfield <tonybloom@users.sourceforge.net>
+ * Copyright (C)      Fernando Vilas <fvilas@iname.com>
+ * Copyright (C) 2014 Christian Dávid <christian-david@web.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef MYMONEYSTORAGESQL_H
 #define MYMONEYSTORAGESQL_H
