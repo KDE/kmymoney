d6ac96c17fe5d4af18e34bf63635a810cafb4ca0
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2016-05-28 19:53:04 +0200

    Allow importing negative sell transaction
    Buy transactions should be negative and sell transactions
    are generally positive but can be negative if fees are
    higher than proceeds. Let OFX, QIF, and CSV importer
    and not statement reader decide about transaction amount signs.
    
    BUG: 351874
    REVIEW: 127983
    
    Signed-off-by: Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>

--- kmymoney/converter/mymoneystatementreader.cpp
+++ kmymoney/converter/mymoneystatementreader.cpp
@@ -830,29 +830,21 @@ void MyMoneyStatementReader::processTransactionEntry(const MyMoneyStatement::Tra
 
       transfervalue = statementTransactionUnderImport.m_amount;
 
-      } else if (statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaBuy)  {
+      } else if ((statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaBuy) ||
+                 (statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaSell)) {
       s1.setAction(MyMoneySplit::ActionBuyShares);
       if (!statementTransactionUnderImport.m_price.isZero())  {
         s1.setPrice(statementTransactionUnderImport.m_price.abs());
       } else if (!statementTransactionUnderImport.m_shares.isZero()) {
-        MyMoneyMoney total = statementTransactionUnderImport.m_amount.abs() - statementTransactionUnderImport.m_fees.abs();
+        MyMoneyMoney total = statementTransactionUnderImport.m_amount + statementTransactionUnderImport.m_fees.abs();
         s1.setPrice((total / statementTransactionUnderImport.m_shares).abs().convert(MyMoneyMoney::precToDenom(KMyMoneyGlobalSettings::pricePrecision())));
       }
-      s1.setShares(statementTransactionUnderImport.m_shares.abs());
-      s1.setValue(statementTransactionUnderImport.m_amount.abs() - statementTransactionUnderImport.m_fees.abs());
-      transfervalue = -(statementTransactionUnderImport.m_amount.abs());
-
-    } else if (statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaSell) {
-      s1.setAction(MyMoneySplit::ActionBuyShares);
-      if (!statementTransactionUnderImport.m_price.isZero())  {
-        s1.setPrice(statementTransactionUnderImport.m_price.abs());
-      } else if (!statementTransactionUnderImport.m_shares.isZero()) {
-        MyMoneyMoney total = statementTransactionUnderImport.m_amount.abs() + statementTransactionUnderImport.m_fees.abs();
-        s1.setPrice((total / statementTransactionUnderImport.m_shares).abs().convert(MyMoneyMoney::precToDenom(KMyMoneyGlobalSettings::pricePrecision())));
-      }
-      s1.setShares(-(statementTransactionUnderImport.m_shares.abs()));
-      s1.setValue(-(statementTransactionUnderImport.m_amount.abs() + statementTransactionUnderImport.m_fees.abs()));
-      transfervalue = statementTransactionUnderImport.m_amount.abs();
+      if (statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaBuy)
+          s1.setShares(statementTransactionUnderImport.m_shares.abs());
+      else
+          s1.setShares(-statementTransactionUnderImport.m_shares.abs());
+      s1.setValue(-(statementTransactionUnderImport.m_amount + statementTransactionUnderImport.m_fees.abs()));
+      transfervalue = statementTransactionUnderImport.m_amount;
 
     } else if ((statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaShrsin) ||
                (statementTransactionUnderImport.m_eAction == MyMoneyStatement::Transaction::eaShrsout)) {
