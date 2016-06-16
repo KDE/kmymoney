3ae1ea665b17ab4297603f1e395f487daac847b9
Christian Dávid <christian-david@web.de>
2016-06-16 16:50:07 +0200

    Fixed issues caused by previous commit

--- kmymoney/payeeidentifier/ibanandbic/tests/internationalaccountidentifier-test.cpp
+++ kmymoney/payeeidentifier/ibanandbic/tests/internationalaccountidentifier-test.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "internationalaccountidentifiertest.h"
+#include "internationalaccountidentifier-test.h"
 #include "../ibanbic.h"
 #include "../ibanbicdata.h"
 
