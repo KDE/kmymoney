12e08b6a64c78aee42f70eb252400a78c0cf44ee
Ralf Habacker <ralf.habacker@freenet.de>
2016-11-15 12:47:22 +0100

    Fix coverity CID 85223: Uninitialized scalar field
    Non-static class member m_fraction is not initialized
    in this constructor nor in any functions that it calls.

--- kmymoney/dialogs/knewequityentrydlg.cpp
+++ kmymoney/dialogs/knewequityentrydlg.cpp
@@ -37,7 +37,8 @@
 #include "mymoneymoney.h"
 
 KNewEquityEntryDlg::KNewEquityEntryDlg(QWidget *parent)
-    : kNewEquityEntryDecl(parent)
+    : kNewEquityEntryDecl(parent),
+      m_fraction(0)
 {
   setModal(true);
   edtFraction->setCalculatorButtonVisible(false);
