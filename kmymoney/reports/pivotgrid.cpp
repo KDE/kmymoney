6f13d7683a764f6fd7765e88a5abe15389e58094
Thomas Baumgart <thb@net-bembel.de>
2016-11-16 08:28:39 +0100

    Slightly improved last commit

--- kmymoney/reports/pivotgrid.cpp
+++ kmymoney/reports/pivotgrid.cpp
@@ -35,9 +35,8 @@ const unsigned PivotOuterGroup::m_kDefaultSortOrder = 100;
 PivotCell::PivotCell(const MyMoneyMoney& value) :
     MyMoneyMoney(value),
     m_stockSplit(MyMoneyMoney::ONE),
-    m_cellUsed(false)
+    m_cellUsed(!value.isZero())
 {
-  m_cellUsed |= !value.isZero();
 }
 
 PivotCell::~PivotCell()
