a9d121906ddfacffbadf6ab2991732c7460f1a1a
Pino Toscano <pino@kde.org>
2017-02-05 12:38:17 +0100

    cmake: remove unused stuff/defines

--- config-kmymoney.h.cmake
+++ config-kmymoney.h.cmake
@@ -3,20 +3,11 @@
 /* Name of package */
 #define PACKAGE "kmymoney"
 
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "KMyMoney"
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine HAVE_STDINT_H 1
-
 /* Define to 1 if you have the <sys/types.h> header file. */
 #cmakedefine HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #cmakedefine HAVE_UNISTD_H 1
-
-/* The size of `long', as computed by sizeof. */
-#cmakedefine SIZEOF_LONG @SIZEOF_LONG@
 
 #cmakedefine KMM_DEBUG 1
 
