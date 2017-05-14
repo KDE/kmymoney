fbd612529ed896765b82f37c912d761aa582b8c2
Thomas Baumgart <thb@net-bembel.de>
2017-05-14 15:16:23 +0200

    Update memo in the other split if requested
    BUG:249844
    FIXED-IN:5.0
    (cherry picked from commit 398304bcf331a322a0fe7e475775aea887c4b565)
    
    and adapted to work on Qt4.8
    
    Signed-off-by: Thomas Baumgart <thb@net-bembel.de>

--- kmymoney/dialogs/transactioneditor.cpp
+++ kmymoney/dialogs/transactioneditor.cpp
@@ -2221,12 +2221,16 @@ bool StdTransactionEditor::createTransaction(MyMoneyTransaction& t, const MyMone
         // in case of a category we simply copy the new value over the old.
         // in case we don't even have an account id, we just skip because
         // the split will be removed later on anyway.
-        if (!s1.memo().isEmpty()) {
+        if (!s1.memo().isEmpty() && s1.memo() != s0.memo()) {
           if (!s1.accountId().isEmpty()) {
             MyMoneyAccount acc = MyMoneyFile::instance()->account(s1.accountId());
-            if (acc.isIncomeExpense()) {
+            if (acc.isIncomeExpense())
               s1.setMemo(s0.memo());
-            }
+            else if (KMessageBox::questionYesNo(m_regForm,
+                                                i18n("Do you want to replace memo<p><i>%1</i></p>with memo<p><i>%2</i></p>in the other split?", s1.memo(), s0.memo()), i18n("Copy memo"),
+                                                KStandardGuiItem::yes(), KStandardGuiItem::no(),
+                                                QLatin1String("CopyMemoOver")) == KMessageBox::Yes)
+              s1.setMemo(s0.memo());
           }
         } else {
           s1.setMemo(s0.memo());
