KDE/kmymoney

commit 593973cd5c6237db698efd27fcc2290309f48da4

BUG:317655 - Re-importing a categories list QIF file results in duplicated categories. Also, when manually creating sub-categories, some may appear two or more times in Categories view. Remove redundant duplicateCategory flag, as per ReviewBoard. Change addedCategoriesList to AccountsModel::Private and reference it through the d-pointer.  Fixed minor style issue.

kmymoney/converter/mymoneyqifreader.cpp
@@ -906,7 +906,7 @@ void MyMoneyQifReader::processCategoryEntry(void)
   }
 
   // check if we can find the account already in the file
-  MyMoneyAccount acc = kmymoney->findAccount(account, MyMoneyAccount());
+  MyMoneyAccount acc = kmymoney->findAccount(account, parentAccount);
 
   // if not, we just create it
   if (acc.id().isEmpty()) {

kmymoney/models/accountsmodel.cpp
@@ -41,6 +41,7 @@
 class AccountsModel::Private
 {
 public:
+
   /**
     * The pimpl.
     */
@@ -63,6 +64,17 @@ public:
   void loadSubAccounts(QStandardItemModel *model, QStandardItem *accountsItem, QStandardItem *favoriteAccountsItem, const QStringList& list) {
     for (QStringList::ConstIterator it_l = list.constBegin(); it_l != list.constEnd(); ++it_l) {
       const MyMoneyAccount& acc = m_file->account(*it_l);
+
+      MyMoneyAccount::accountTypeE acctype = acc.accountType();
+      if ((acctype == MyMoneyAccount::Expense) || (acctype == MyMoneyAccount::Income)) {
+        if (m_addedCategoriesList.contains(acc.id())) {
+          //  Has been added already
+          return;
+        } else {
+          //  Add it to list
+          m_addedCategoriesList << acc.id();
+        }
+      }
 
       QStandardItem *item = new QStandardItem(acc.name());
       accountsItem->appendRow(item);
@@ -341,6 +353,12 @@ public:
     * Used to set the reconciliation flag.
     */
   MyMoneyAccount m_reconciledAccount;
+  /**
+    * Used to maintain a list of categories added
+    * during recursion through a category tree,
+    * before its completion.
+    */
+  QStringList m_addedCategoriesList;
 };
 
 const QString AccountsModel::favoritesAccountId("Favorites");
@@ -404,6 +422,7 @@ void AccountsModel::load()
 {
   //::timetrace("Start AccountsModel::load");
   QStandardItem *rootItem = invisibleRootItem();
+  d->m_addedCategoriesList.clear();
 
   QFont font;
   font.setBold(true);

commit 54a262f80d511ad16823c1c88f64fd5dadda9c44

Fix 'Expand category doesnt work after edit the parent category'.
Revert "BUG:317655 - Re-importing a categories list QIF file results in duplicated"

This reverts commit 593973cd5c6237db698efd27fcc2290309f48da4 which caused this bug.

BUG:381581
FIXED-IN:4.8.1

kmymoney/converter/mymoneyqifreader.cpp
@@ -911,7 +911,7 @@ void MyMoneyQifReader::processCategoryEntry()
   }
 
   // check if we can find the account already in the file
-  MyMoneyAccount acc = kmymoney->findAccount(account, parentAccount);
+  MyMoneyAccount acc = kmymoney->findAccount(account, MyMoneyAccount());
 
   // if not, we just create it
   if (acc.id().isEmpty()) {

kmymoney/models/accountsmodel.cpp
@@ -41,7 +41,6 @@
 class AccountsModel::Private
 {
 public:
-
   /**
     * The pimpl.
     */
@@ -64,17 +63,6 @@ public:
   void loadSubAccounts(QStandardItemModel *model, QStandardItem *accountsItem, QStandardItem *favoriteAccountsItem, const QStringList& list) {
     for (QStringList::ConstIterator it_l = list.constBegin(); it_l != list.constEnd(); ++it_l) {
       const MyMoneyAccount& acc = m_file->account(*it_l);
-
-      MyMoneyAccount::accountTypeE acctype = acc.accountType();
-      if ((acctype == MyMoneyAccount::Expense) || (acctype == MyMoneyAccount::Income)) {
-        if (m_addedCategoriesList.contains(acc.id())) {
-          //  Has been added already
-          return;
-        } else {
-          //  Add it to list
-          m_addedCategoriesList << acc.id();
-        }
-      }
 
       QStandardItem *item = new QStandardItem(acc.name());
       accountsItem->appendRow(item);
@@ -362,12 +350,6 @@ public:
     * Used to set the reconciliation flag.
     */
   MyMoneyAccount m_reconciledAccount;
-  /**
-    * Used to maintain a list of categories added
-    * during recursion through a category tree,
-    * before its completion.
-    */
-  QStringList m_addedCategoriesList;
 };
 
 const QString AccountsModel::favoritesAccountId("Favorites");
@@ -430,7 +412,6 @@ void AccountsModel::init()
 void AccountsModel::load()
 {
   QStandardItem *rootItem = invisibleRootItem();
-  d->m_addedCategoriesList.clear();
 
   QFont font;
   font.setBold(true);