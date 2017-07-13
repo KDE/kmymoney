4f6a28f91c9779afefc91ab9edc6a9c220be6316
Ralf Habacker <ralf.habacker@freenet.de>
2017-07-13 11:40:20 +0200

    Be KF5 save.

--- kmymoney/kmymoney.cpp
+++ kmymoney/kmymoney.cpp
@@ -2702,7 +2702,7 @@ bool KMyMoneyApp::slotBackupWriteFile()
   d->m_proc.clearProgram();
 #ifdef Q_OS_WIN
   d->m_proc << "cmd.exe" << "/c" << "copy" << "/b" << "/y";
-  d->m_proc << (QDir::toNativeSeparators(d->m_fileName.path(KUrl::LeaveTrailingSlash))+"+ nul") << QDir::toNativeSeparators(backupfile);
+  d->m_proc << (QDir::toNativeSeparators(d->m_fileName.toLocalFile()) + "+ nul") << QDir::toNativeSeparators(backupfile);
 #else
   d->m_proc << "cp" << "-f";
   d->m_proc << d->m_fileName.path(KUrl::LeaveTrailingSlash) << backupfile;
