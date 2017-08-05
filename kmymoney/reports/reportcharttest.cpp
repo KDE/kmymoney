51529f756008f2c879a92b0b3fef72a716e51e2b
Ralf Habacker <ralf.habacker@freenet.de>
2017-08-05 10:43:50 +0200

    Add missing copyright header
    bugzilla complaints about this.
    
    CCBUG:382427

--- kmymoney/reports/reportcharttest.cpp
+++ kmymoney/reports/reportcharttest.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+                          reportcharttest.cpp
+                          -------------------
+    copyright            : (C) 2017 by Ralf Habacker
+    email                : ralf.habacker@freenet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QApplication>
 #include <KDChartWidget>
 #include <KDChartLineDiagram>
