38f19da88c783426654364ec420afbe19874d9d0
Thomas Baumgart <thb@net-bembel.de>
2018-01-20 10:24:15 +0100

    Fix 'missing assignment' error after changing invest transaction to reinvest
    The problem was caused by the fact that assetAccountSplit was not
    disabled from the previous activity type.
    
    BUG:388700
    
    (cherry picked from commit 6ae7e1844e0511636e19bb6bb7f36de6242fcb61)

--- kmymoney/dialogs/investactivities.cpp
+++ kmymoney/dialogs/investactivities.cpp
@@ -674,6 +674,7 @@ bool Reinvest::createTransaction(MyMoneyTransaction& t, MyMoneySplit& s0, MyMone
     qDebug("more or less than one interest split in Reinvest::createTransaction. Not created.");
     return false;
   }
+  assetAccountSplit.setAccountId(QString());
 
   MyMoneySplit& s1 = interestSplits[0];
 
