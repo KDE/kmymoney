6e3e8f3d8250ff954bcff552dc0021992935405c
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-06-02 15:05:05 +0200

    Format models headers according to the KDE guidelines
    Purpose of this patch is to tidy up source code headers. Right now we've
    got three different types of headers (accountsviewproxymodel.cpp,
    payeeidentifier.cpp, kcurrecycalculator.cpp). This patch will
    standardize them to one according to
    https://community.kde.org/Policies/Licensing_Policy#GPL_Header
    
    Moreover, there are some names in the headers, which have not event
    touched the file, so it's false authorship. Those names has been
    removed. List of the names to remove has been taken from:
    1) CVS logs (http://kmymoney2.cvs.sourceforge.net/),
    2) git logs,
    3) KMM Sourceforge developer mailing list
    (https://sourceforge.net/p/kmymoney2/mailman/kmymoney2-developer/)
    4) CVS stats
    (http://kmymoney2.sourceforge.net/kmymoney2-stats/authors.html)
    5) ChangeLog of CVS
    
    Differential Revision: https://phabricator.kde.org/D13159

--- kmymoney/models/ledgerschedule.h
+++ kmymoney/models/ledgerschedule.h
@@ -1,20 +1,20 @@
-/***************************************************************************
-                          ledgerschedule.h
-                             -------------------
-    begin                : Sat Aug 8 2015
-    copyright            : (C) 2015 by Thomas Baumgart
-    email                : Thomas Baumgart <tbaumgart@kde.org>
-                           (C) 2017 by Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * Copyright 2014-2017  Thomas Baumgart <tbaumgart@kde.org>
+ * Copyright 2017       Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LEDGERSCHEDULE_H
 #define LEDGERSCHEDULE_H
