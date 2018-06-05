3c5c5070b03762f36f83f88f8204b6a647ed86cd
Thomas Baumgart <thb@net-bembel.de>
2018-06-05 17:36:42 +0200

    Write correct number of columns for investment transactions to csv file
    Cherry-picked from 3a89db9fac1294a1bc3cfe09551caeef40572bde and adjusted
    to master

--- kmymoney/plugins/csv/export/csvwriter.cpp
+++ kmymoney/plugins/csv/export/csvwriter.cpp
@@ -311,7 +311,6 @@ void CsvWriter::writeInvestmentEntry(const MyMoneyTransaction& t, const int coun
           strStatus.clear();
           break;
       }
-      strStatus += m_separator;
     }
   }
   //
@@ -418,6 +417,16 @@ void CsvWriter::writeInvestmentEntry(const MyMoneyTransaction& t, const int coun
       strAccName = format(acc.name());
       strAction += m_separator;
     }
+
+   if (strAmount.isEmpty())
+      strAmount = m_separator;
+
+   if (strQuantity.isEmpty())
+      strQuantity = m_separator;
+
+    if (strPrice.isEmpty())
+      strPrice = m_separator;
+
     if (strCheckingAccountName.isEmpty()) {
       strCheckingAccountName = m_separator;
     }
