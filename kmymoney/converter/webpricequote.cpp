18cf542243def84fd2acee88170a81262a3209e0
Thomas Baumgart <thb@net-bembel.de>
2018-07-23 21:03:52 +0200

    Support online web source providing price pairs without decimal
    Manually cherry-picked from 3cc0235040fcfa4566cbb262e46348a0721c012d and
    adjusted
    
    BUG:396797

--- kmymoney/converter/webpricequote.cpp
+++ kmymoney/converter/webpricequote.cpp
@@ -565,7 +565,7 @@ const QMap<QString, WebPriceQuoteSource> WebPriceQuote::defaultQuoteSources()
                                           QString(),
                                           "https://fx-rate.net/([^/]+/[^/]+)",
                                           WebPriceQuoteSource::identifyBy::Symbol,
-                                          "1\\s[^=]+\\s=</span><br\\s/>\\s([^\\s]+)",
+                                          "1[ a-zA-Z]+=</span><br */?> *(\\d+|\\d+\\.\\d+)",
                                           "updated\\s\\d+:\\d+:\\d+\\(\\w+\\)\\s+(\\d{1,2}/\\d{2}/\\d{4})",
                                           "%d/%m/%y",
                                           true // skip HTML stripping
