60e2d8cfd3a774b360ce8aadbd993c50fed404ef
Thomas Baumgart <thb@net-bembel.de>
2019-02-03 14:33:46 +0100

    Do not allow to enter transactions prior to account opening date
    The transaction editor in the standard account ledger does not allow to
    enter transactions that reference accounts that have an opening date
    later than the transaction date.
    
    The investment transaction editor is missing this feature and one could
    enter transactions with a date prior to the opening date of the
    investment or asset account referenced in the transaction.
    
    Moreover, the opening date of an invest account cannot be changed by the
    user but got updated to a matching value when the consistency check was
    run (e.g. as part of file save). The user was informed and was
    surprised.
    
    This change adds an automatic update of the opening date for the invest
    account if needed when the transaction is entered. Also, it does not
    allow to enter the transaction, colorizes the date and presents a
    tooltip on the date widget just like for the standard transaction
    editor.
    
    BUG: 403886
    FIXED-IN: 5.0.4

--- kmymoney/dialogs/investtransactioneditor.cpp
+++ kmymoney/dialogs/investtransactioneditor.cpp
@@ -357,7 +357,8 @@ void InvestTransactionEditor::createEditWidgets()
   connect(activity, &KMyMoneyActivityCombo::activitySelected, this, &InvestTransactionEditor::slotUpdateActivity);
   connect(activity, &KMyMoneyActivityCombo::activitySelected, this, &InvestTransactionEditor::slotUpdateButtonState);
 
-  d->m_editWidgets["postdate"] = new KMyMoneyDateInput;
+  auto postDate = d->m_editWidgets["postdate"] = new KMyMoneyDateInput;
+  connect(postDate, SIGNAL(dateChanged(QDate)), this, SLOT(slotUpdateButtonState()));
 
   auto security = new KMyMoneySecurity;
   security->setObjectName("security");
@@ -776,6 +777,36 @@ bool InvestTransactionEditor::isComplete(QString& reason) const
 {
   Q_D(const InvestTransactionEditor);
   reason.clear();
+
+  auto postDate = dynamic_cast<KMyMoneyDateInput*>(d->m_editWidgets["postdate"]);
+  if (postDate) {
+    QDate accountOpeningDate = d->m_account.openingDate();
+    auto asset = dynamic_cast<KMyMoneyCategory*>(haveWidget("asset-account"));
+    if (asset && asset->isVisible()) {
+      if (!isMultiSelection() || !asset->currentText().isEmpty()) {
+        const auto assetId = asset->selectedItem();
+        if (!assetId.isEmpty()) {
+          try {
+            const auto acc = MyMoneyFile::instance()->account(assetId);
+            if (acc.openingDate() > accountOpeningDate)
+              accountOpeningDate = acc.openingDate();
+          } catch(MyMoneyException& e) {
+            qDebug() << "opening date check failed on account" << assetId << e.what();
+          }
+        }
+      }
+    }
+
+    if (postDate->date().isValid() && (postDate->date() < accountOpeningDate)) {
+      postDate->markAsBadDate(true, KMyMoneySettings::schemeColor(SchemeColor::Negative));
+      reason = i18n("Cannot enter transaction with postdate prior to account's opening date.");
+      postDate->setToolTip(reason);
+      return false;
+    }
+    postDate->markAsBadDate();
+    postDate->setToolTip(QString());
+  }
+
   return d->m_activity->isComplete(reason);
 }
 
