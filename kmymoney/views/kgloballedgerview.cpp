5008fc800e8806522263a9c9b5cc1cc5d74c7ae5
Thomas Baumgart <thb@net-bembel.de>
2019-06-08 15:36:39 +0200

    Select amount when changing focus using the tab/backtab key
    The previous commit showed a problem in the transaction editor such that
    the amount was not selected when the focus was changed to the amount
    field using the tab or backtab key. This used to work before with the
    KMyMoneyEdit object.
    
    This change fixes the problem by passing the reason as part of the focus
    change.

--- kmymoney/views/kgloballedgerview.cpp
+++ kmymoney/views/kgloballedgerview.cpp
@@ -1015,7 +1015,7 @@ bool KGlobalLedgerView::focusNextPrevChild(bool next)
 
         if (((w->focusPolicy() & Qt::TabFocus) == Qt::TabFocus) && w->isVisible() && w->isEnabled()) {
           // qDebug() << "Set focus to" << w->objectName();
-          w->setFocus();
+          w->setFocus(next ? Qt::TabFocusReason: Qt::BacktabFocusReason);
           rc = true;
           break;
         }
