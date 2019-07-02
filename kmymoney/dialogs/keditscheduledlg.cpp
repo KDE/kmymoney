KDE/kmymoney

commit 8841ae39d15b5ddb916a151d7bc20415862fe34d

Change KMyMoneyEdit to AmountEdit
Summary:
Refactor KMyMoney to use just AmountEdit

Test Plan: "make test" and explore the interface

Reviewers: #kmymoney, tbaumgart

Reviewed By: #kmymoney, tbaumgart

Subscribers: wojnilowicz, brunourias, tbaumgart

Differential Revision: https://phabricator.kde.org/D20887

kmymoney/dialogs/kmymoneysplittable.cpp
@@ -52,7 +52,7 @@
 #include "mymoneyaccount.h"
 #include "mymoneyfile.h"
 #include "mymoneyprice.h"
-#include "kmymoneyedit.h"
+#include "amountedit.h"
 #include "kmymoneycategory.h"
 #include "kmymoneyaccountselector.h"
 #include "kmymoneylineedit.h"
@@ -145,7 +145,7 @@ public:
     * The widget will be created and destroyed dynamically in createInputWidgets()
     * and destroyInputWidgets().
     */
-  QPointer<KMyMoneyEdit>     m_editAmount;
+  QPointer<AmountEdit>     m_editAmount;
 
   /**
     * This member keeps the tab order for the above widgets
@@ -516,7 +516,7 @@ void KMyMoneySplitTable::mouseDoubleClickEvent(QMouseEvent *e)
       break;
 
     case 3:
-      editWidget = d->m_editAmount->lineedit();
+      editWidget = d->m_editAmount;
       break;
 
     default:
@@ -631,7 +631,7 @@ void KMyMoneySplitTable::slotUpdateData(const MyMoneyTransaction& t)
       amountTxt.clear();
 
     unsigned width = fontMetrics().width(amountTxt);
-    KMyMoneyEdit* valfield = new KMyMoneyEdit();
+    AmountEdit* valfield = new AmountEdit();
     valfield->setMinimumWidth(width);
     width = valfield->minimumSizeHint().width();
     delete valfield;
@@ -952,9 +952,9 @@ KMyMoneyCategory* KMyMoneySplitTable::createEditWidgets(bool setFocus)
   d->m_tabOrderWidgets.clear();
 
   // create the widgets
-  d->m_editAmount = new KMyMoneyEdit(0);
+  d->m_editAmount = new AmountEdit;
   d->m_editAmount->setFont(cellFont);
-  d->m_editAmount->setResetButtonVisible(false);
+  d->m_editAmount->setCalculatorButtonVisible(true);
   d->m_editAmount->setPrecision(d->m_precision);
 
   d->m_editCategory = new KMyMoneyCategory();
@@ -1027,7 +1027,7 @@ KMyMoneyCategory* KMyMoneySplitTable::createEditWidgets(bool setFocus)
   // don't allow automatically calculated values to be modified
   if (d->m_split.value() == MyMoneyMoney::autoCalc) {
     d->m_editAmount->setEnabled(false);
-    d->m_editAmount->loadText("will be calculated");
+    d->m_editAmount->setText("will be calculated");
   } else
     d->m_editAmount->setValue(d->m_split.value());
 

commit 0ce282a4b21f8490672cf42905167f6952dfd83c

Select amount when changing focus using the tab/backtab key
Commit 8841ae39d15b5ddb916a151d7bc20415862fe34d introduced a problem
such that the amount was not selected when the focus was changed to the
amountfield using the tab or backtab key. This used to work before with
the KMyMoneyEdit object.

For the standard transaction editor this was fixed by commit
5008fc800e8806522263a9c9b5cc1cc5d74c7ae5 but there are more locations in
the code where this change is necessary.

This change adds the fix to cover all locations which show the above
problem.

BUG: 409371

kmymoney/dialogs/keditscheduledlg.cpp
@@ -609,7 +609,7 @@ bool KEditScheduleDlg::focusNextPrevChild(bool next)
 
       if (((w->focusPolicy() & Qt::TabFocus) == Qt::TabFocus) && w->isVisible() && w->isEnabled()) {
         // qDebug("Selecting '%s' as focus", qPrintable(w->objectName()));
-        w->setFocus();
+        w->setFocus(next ? Qt::TabFocusReason: Qt::BacktabFocusReason);
         rc = true;
       }
     } while (rc == false);

kmymoney/dialogs/kenterscheduledlg.cpp
@@ -394,7 +394,7 @@ bool KEnterScheduleDlg::focusNextPrevChild(bool next)
 
     if (((w->focusPolicy() & Qt::TabFocus) == Qt::TabFocus) && w->isVisible() && w->isEnabled()) {
       // qDebug("Selecting '%s' as focus", w->className());
-      w->setFocus();
+      w->setFocus(next ? Qt::TabFocusReason: Qt::BacktabFocusReason);
       rc = true;
     }
   }

kmymoney/dialogs/kmymoneysplittable.cpp
@@ -1127,7 +1127,7 @@ bool KMyMoneySplitTable::focusNextPrevChild(bool next)
 
       if (((w->focusPolicy() & Qt::TabFocus) == Qt::TabFocus) && w->isVisible() && w->isEnabled()) {
         // qDebug("Selecting '%s' as focus", w->className());
-        w->setFocus();
+        w->setFocus(next ? Qt::TabFocusReason: Qt::BacktabFocusReason);
         rc = true;
       }
     }