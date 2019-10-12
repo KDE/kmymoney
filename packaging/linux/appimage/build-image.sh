81ba589bf90e2c61a246fa5cf1fff04a558f34d1
Thomas Baumgart <thb@net-bembel.de>
2019-10-12 15:26:12 +0200

    Fix source path of plugins
    one 'usr' too much

--- packaging/linux/appimage/build-image.sh
+++ packaging/linux/appimage/build-image.sh
@@ -52,7 +52,7 @@ cp -r -v $DEPS_INSTALL_PREFIX/share/kservices5 $APPDIR/usr/share
 cp -r -v $DEPS_INSTALL_PREFIX/share/mime $APPDIR/usr/share
 # cp -r -v $DEPS_INSTALL_PREFIX/translations $APPDIR/usr/
 cp -r -v $DEPS_INSTALL_PREFIX/openssl/lib/*  $APPDIR/usr/lib
-cp -r -v $DEPS_INSTALL_PREFIX/usr/plugins/* $APPDIR/usr/plugins
+cp -r -v $DEPS_INSTALL_PREFIX/plugins/* $APPDIR/usr/plugins
 
 # @todo we still need the following program from /bin or /usr/bin:
 #  true, mount, umount (backup)
