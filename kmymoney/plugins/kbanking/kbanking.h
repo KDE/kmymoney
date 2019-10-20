72abb79eb2ec970f13ed7efac203196430a92cfe
Thomas Baumgart <thb@net-bembel.de>
2019-10-20 17:37:58 +0200

    KBanking differentiates between account types
    In case an institution uses the same account number for e.g. for
    sub-accounts and investment depots KMyMoney was not able to
    differentiate between the two accounts.
    
    This change adds the type to the value used for finding the right
    account in the KMyMoney account list.
    
    Also, information about investments will be added to the statement.
    
    Original author: Holger Magnussen <holger@muscate-magnussen.de>

--- kmymoney/plugins/kbanking/kbanking.h
+++ kmymoney/plugins/kbanking/kbanking.h
@@ -1,25 +1,22 @@
-/***************************************************************************
- *   Copyright 2004  Martin Preuss aquamaniac@users.sourceforge.net        *
- *   Copyright 2009  Cristian Onet onet.cristian@gmail.com                 *
- *   Copyright 2010  Thomas Baumgart ipwizard@users.sourceforge.net        *
- *   Copyright 2016  Christian David christian-david@web.de                *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU General Public License as        *
- *   published by the Free Software Foundation; either version 2 of        *
- *   the License or (at your option) version 3 or any later version        *
- *   accepted by the membership of KDE e.V. (or its successor approved     *
- *   by the membership of KDE e.V.), which shall act as a proxy            *
- *   defined in Section 14 of version 3 of the license.                    *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>  *
- ***************************************************************************/
+/*
+ * Copyright 2004       Martin Preuss aquamaniac@users.sourceforge.net
+ * Copyright 2009       Cristian Onet onet.cristian@gmail.com
+ * Copyright 2010-2019  Thomas Baumgart tbaumgart@kde.org
+ * Copyright 2016       Christian David christian-david@web.de
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef KBANKING_H
 #define KBANKING_H
 
@@ -236,7 +233,10 @@ protected:
   bool askMapAccount(const MyMoneyAccount& acc);
   QString mappingId(const MyMoneyObject& object) const;
 
-  bool importAccountInfo(AB_IMEXPORTER_ACCOUNTINFO *ai, uint32_t flags) final override;
+  bool importAccountInfo(AB_IMEXPORTER_CONTEXT *ctx, AB_IMEXPORTER_ACCOUNTINFO *ai, uint32_t flags) final override;
+  void _slToStatement(MyMoneyStatement &ks,
+                      const MyMoneyAccount&,
+                      const AB_SECURITY *sy);
   void _xaToStatement(MyMoneyStatement &ks,
                       const MyMoneyAccount&,
                       const AB_TRANSACTION *t);
