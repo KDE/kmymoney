1989a4296d070c9ad962578815ab4fa169f61706
Thomas Baumgart <thb@net-bembel.de>
2020-03-26 16:36:58 +0100

    Fix local filename on windows for ofx banking
    BUG: 396286
    FIXED-IN: 5.0.9

--- kmymoney/plugins/ofx/import/ofxpartner.h
+++ kmymoney/plugins/ofx/import/ofxpartner.h
@@ -1,19 +1,19 @@
-/***************************************************************************
-                             ofxpartner.h
-                             ----------
-    begin                : Fri Jan 23 2009
-    copyright            : (C) 2009 by Thomas Baumgart
-    email                : Thomas Baumgart <ipwizard@users.sourceforge.net>
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * Copyright 2009-2020  Thomas Baumgart <tbaumgart@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef OFXPARTNER_H
 #define OFXPARTNER_H
@@ -92,7 +92,7 @@ protected Q_SLOTS:
 private:
   class Private;
   Private*          d;
-  QUrl              m_dst;
+  QString           m_dst;
   QFile             m_file;
   int               m_error;
   KIO::TransferJob* m_postJob;
