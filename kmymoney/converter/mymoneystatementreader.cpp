5bcf3cf9db2f689aefc7d9c3459ac0a496075c85
Dawid Wróbel <me@dawidwrobel.com>
2020-04-12 02:00:26 -0400

    Revert the fix which broke duplicate transaction detection between imports.
    This reverts commit 295d003c977ee67e2af7d76a14a39869c599ac9e.
    
    CCBUG: 419975

--- kmymoney/converter/mymoneystatementreader.cpp
+++ kmymoney/converter/mymoneystatementreader.cpp
@@ -1279,8 +1279,9 @@ void MyMoneyStatementReader::processTransactionEntry(const MyMoneyStatement::Tra
     result = existingTrMatchFinder.findMatch(transactionUnderImport, s1);
     if (result != TransactionMatchFinder::MatchNotFound) {
       MyMoneyTransaction matchedTransaction = existingTrMatchFinder.getMatchedTransaction();
-      if (!matchedTransaction.isImported()                                 // don't match with just imported transaction
-          && (result == TransactionMatchFinder::MatchDuplicate || result == TransactionMatchFinder::MatchPrecise)) {
+      if (result == TransactionMatchFinder::MatchDuplicate
+      || !matchedTransaction.isImported()
+      || result == TransactionMatchFinder::MatchPrecise) { // don't match with just imported transaction
         MyMoneySplit matchedSplit = existingTrMatchFinder.getMatchedSplit();
         handleMatchingOfExistingTransaction(matcher, matchedTransaction, matchedSplit, transactionUnderImport, s1, result);
         return;
