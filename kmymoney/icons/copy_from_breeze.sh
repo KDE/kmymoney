23432d73db3856afe1a808eecd1d1aebe497dd5e
Dawid Wróbel <me@dawidwrobel.com>
2020-10-02 14:17:43 -0400

    copy_from_breeze.sh: copy index.theme, mime files

--- kmymoney/icons/copy_from_breeze.sh
+++ kmymoney/icons/copy_from_breeze.sh
@@ -4,12 +4,16 @@
 # where breeze-icon\ is a path to a root folder of a cloned breeze-icon git repository
 # (must contain "icons" and "icons-dark" subfolders).
 
-for i in $(sed -E -n -e "s/^.*{IconSet\:\:(Breeze|Common), QStringLiteral\(\"(.*)\"\)}.*/\2/p" icons.cpp); do
+used_in_code=$(sed -E -n -e "s/^.*{IconSet\:\:(Breeze|Common), QStringLiteral\(\"(.*)\"\)}.*/\2/p" icons.cpp)
+additional=" index application-x-kmymoney"
+all_icons=$used_in_code$additional
+
+for i in $all_icons; do
   for f in $(find $1 -name "$i.*"); do
      r=${f/#$1\/icons/breeze}
      d=${r%/*}
      [ -d "$d" ] || mkdir -p "$d"
      echo "copying $f to $r"
      cp "$f" "$r"
-   done  
+   done
 done
