b16a261fca35042abd1f4a1b2812316c6002461d
Thomas Baumgart <thb@net-bembel.de>
2020-10-31 12:09:51 +0100

    Adjusted file headers to current format

--- kmymoney/plugins/views/forecast/kforecastview.cpp
+++ kmymoney/plugins/views/forecast/kforecastview.cpp
@@ -1,19 +1,21 @@
-/***************************************************************************
-                          kforecastview.cpp
-                             -------------------
-    copyright            : (C) 2007 by Alvaro Soliverez
-    email                : asoliverez@gmail.com
-                           (C) 2017 Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * Copyright (C) 2007      Alvaro Soliverez <asoliverez@gmail.com>
+ * Copyright (C) 2017      Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+ * Copyright (C) 2020      Thomas Baumgart <tbaumgart@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "kforecastview_p.h"
 
