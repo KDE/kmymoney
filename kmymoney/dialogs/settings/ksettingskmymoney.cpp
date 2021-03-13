e0cf2bcaf1f825c903fc260811322da846ff839b
Thomas Baumgart <thb@net-bembel.de>
2021-03-13 18:41:19 +0100

    Use same icon for ledger view and settings

--- kmymoney/dialogs/settings/ksettingskmymoney.cpp
+++ kmymoney/dialogs/settings/ksettingskmymoney.cpp
@@ -40,7 +40,7 @@ KSettingsKMyMoney::KSettingsKMyMoney(QWidget *parent, const QString &name, KCore
 
   addPage(generalPage, i18nc("General settings", "General"), Icons::get(Icon::PreferencesGeneral).name());
   addPage(homePage, i18n("Home"), Icons::get(Icon::Home).name());
-  addPage(registerPage, i18nc("Ledger view settings", "Ledger"), Icons::get(Icon::Ledger).name());
+  addPage(registerPage, i18nc("Ledger view settings", "Ledger"), Icons::get(Icon::Ledgers).name());
   addPage(schedulesPage, QString(i18n("Scheduled\ntransactions")).replace(QLatin1Char('\n'), QString::fromUtf8("\xe2\x80\xa8")), Icons::get(Icon::Schedule).name());
   addPage(onlineQuotesPage, i18n("Online Quotes"), Icons::get(Icon::PreferencesNetwork).name());
   addPage(colorsPage, i18n("Colors"), Icons::get(Icon::PreferencesColors).name());
