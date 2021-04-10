KDE/kmymoney

commit 76b258aeca5ce18499db6bb31a184afa94497d87

CSV Importer: allow other text file extensions
Bank statements, even if correctly CSV-formatted, can often be provided
with a different extension – or none at all. So as long as we're able to
open the file as text, we should allow user to choose it.

Also use mime types for an implicit i18n.


(cherry picked from commit 403529b33ba9d69762392ae1649361d465fd587d)

kmymoney/plugins/csv/import/csvimporter.cpp
@@ -79,16 +79,13 @@ QString CSVImporter::formatName() const
 
 QString CSVImporter::formatFilenameFilter() const
 {
-    return "*.csv";
+    return "*.csv *.txt";
 }
 
 bool CSVImporter::isMyFormat(const QString& filename) const
 {
-    // filename is considered a CSV file if it can be opened
-    // and the filename ends in ".csv" (case does not matter).
     QFile f(filename);
-    return filename.endsWith(QLatin1String(".csv"), Qt::CaseInsensitive)
-           && f.open(QIODevice::ReadOnly | QIODevice::Text);
+    return f.open(QIODevice::ReadOnly | QIODevice::Text);
 }
 
 void CSVImporter::startWizardRun()

kmymoney/plugins/csv/import/core/csvimportercore.cpp
@@ -1783,8 +1783,8 @@ bool CSVFile::getInFileName(QString inFileName)
         fileInfo = QFileInfo(QDir::homePath());
 
     QPointer<QFileDialog> dialog = new QFileDialog(nullptr, QString(),
-            fileInfo.absoluteFilePath(),
-            i18n("CSV Files (*.csv)"));
+            fileInfo.absoluteFilePath());
+    dialog->setMimeTypeFilters({"text/csv", "text/plain", "application/octet-stream"});
     dialog->setFileMode(QFileDialog::ExistingFile);
     QUrl url;
     if (dialog->exec() == QDialog::Accepted)

commit bb53818248eee75f0296d5a61f0d692828ed0fc3

CSV Importer: fix a bug introduced by 76b258ae
isMyFormat() must be specific about the files it can handle

kmymoney/plugins/csv/import/csvimporter.cpp
@@ -85,7 +85,10 @@ QString CSVImporter::formatFilenameFilter() const
 bool CSVImporter::isMyFormat(const QString& filename) const
 {
     QFile f(filename);
-    return f.open(QIODevice::ReadOnly | QIODevice::Text);
+    return (filename.endsWith(QLatin1String(".csv"), Qt::CaseInsensitive) ||
+        filename.endsWith(QLatin1String(".tsv"), Qt::CaseInsensitive) ||
+        filename.endsWith(QLatin1String(".txt"), Qt::CaseInsensitive))
+        && f.open(QIODevice::ReadOnly | QIODevice::Text);
 }
 
 void CSVImporter::startWizardRun()