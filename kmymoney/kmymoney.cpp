121cbddee07aca03df02ee31f5b20ea54449906c
Thomas Baumgart <thb@net-bembel.de>
2021-06-17 19:05:28 +0200

    Avoid using QStringBuilder

--- kmymoney/kmymoney.cpp
+++ kmymoney/kmymoney.cpp
@@ -1331,7 +1331,7 @@ KMyMoneyApp::KMyMoneyApp(QWidget* parent) :
     new KmymoneyAdaptor(this);
     QDBusConnection::sessionBus().registerObject("/KMymoney", this);
     QDBusConnection::sessionBus().interface()->registerService(
-        "org.kde.kmymoney-" + QString::number(platformTools::processId()), QDBusConnectionInterface::DontQueueService);
+        QStringLiteral("org.kde.kmymoney-%1").arg(QString::number(platformTools::processId()), QDBusConnectionInterface::DontQueueService));
 #endif
     // Register the main engine types used as meta-objects
     qRegisterMetaType<MyMoneyMoney>("MyMoneyMoney");
