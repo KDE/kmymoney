0a8b2bfd187026d5e1e1260334162f5ece5dc0ea
Thomas Baumgart <thb@net-bembel.de>
2021-08-07 20:39:41 +0200

    Fix previous commit

--- kmymoney/plugins/views/reports/kreportsview.cpp
+++ kmymoney/plugins/views/reports/kreportsview.cpp
@@ -331,9 +331,7 @@ void KReportsView::slotExportView()
         dialog->setMimeTypeFilters({QStringLiteral("text/csv"), QStringLiteral("text/html")});
         dialog->setFileMode(QFileDialog::AnyFile);
         dialog->setAcceptMode(QFileDialog::AcceptSave);
-        if (auto tab = dynamic_cast<KReportTab*>(d->m_reportTabWidget->currentWidget())) {
-            dialog->selectFile(tab->report().name());
-        }
+        dialog->selectFile(tab->report().name());
 
         QUrl newURL;
         QString selectedMimeType;
