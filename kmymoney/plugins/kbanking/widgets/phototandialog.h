fb3143f73811caf8f5187b50ac6fd0b8f1581dc4
Thomas Baumgart <thb@net-bembel.de>
2022-01-17 19:02:55 +0100

    Resolve moc warning

--- kmymoney/plugins/kbanking/widgets/phototandialog.h
+++ kmymoney/plugins/kbanking/widgets/phototandialog.h
@@ -21,8 +21,8 @@ class photoTanDialog;
 class photoTanDialog : public QDialog
 {
     Q_OBJECT
-    Q_PROPERTY(QString infoText READ infoText() WRITE setInfoText CONSTANT)
-    Q_PROPERTY(QPixmap picture READ picture() WRITE setPicture CONSTANT)
+    Q_PROPERTY(QString infoText READ infoText() WRITE setInfoText)
+    Q_PROPERTY(QPixmap picture READ picture() WRITE setPicture)
 
 public:
     explicit photoTanDialog(QWidget* parent = 0);
