aecff7cf0a219618255bfc18401f933ee10580ff
Thomas Baumgart <thb@net-bembel.de>
2022-03-25 15:18:39 +0100

    Persist exact name matching
    CCBUG: 424377

--- kmymoney/mymoney/mymoneypayee.cpp
+++ kmymoney/mymoney/mymoneypayee.cpp
@@ -268,7 +268,10 @@ void MyMoneyPayee::setMatchData(eMyMoney::Payee::MatchType type, bool ignorecase
             const QRegularExpression validKeyRegExp(QLatin1String("[^ ]"));
             const auto filteredKeys = keys.filter(validKeyRegExp);
             d->m_matchKey = filteredKeys.join(QLatin1Char('\n'));
-        } else if(type == eMyMoney::Payee::MatchType::NameExact) {
+        } else if ((type == eMyMoney::Payee::MatchType::Name) && (keys.count() == 1) && (keys.at(0) == QLatin1String("^$"))) {
+            type = eMyMoney::Payee::MatchType::NameExact;
+            d->m_matchKey = QLatin1String("^$");
+        } else if (type == eMyMoney::Payee::MatchType::NameExact) {
             d->m_matchKey = QLatin1String("^$");
         }
     }
