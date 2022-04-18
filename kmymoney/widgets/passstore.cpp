f201b0de7a4c4871478b0d1413671a4f6b567b2a
Thomas Baumgart <thb@net-bembel.de>
2022-04-18 11:45:23 +0200

    Added initial support to extract passwords from external vault
    This implements a method to extract passwords from a vault maintained by
    pass (see https://www.passwordstore.org) using our existing GPG
    integration in kmm_gpgfile.

---
+++ kmymoney/widgets/passstore.cpp
@@ -0,0 +1,101 @@
+/*
+    SPDX-FileCopyrightText: 2022 Thomas Baumgart <tbaumgart@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "passstore.h"
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <QAction>
+#include <QDir>
+#include <QFileInfo>
+#include <QLineEdit>
+#include <QTextStream>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+#include <KLocalizedString>
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+#include "icons.h"
+#include "kgpgfile.h"
+
+using namespace Icons;
+
+class PassStorePrivate
+{
+public:
+    PassStorePrivate(PassStore* qq)
+        : q_ptr(qq)
+        , m_lineEdit(nullptr)
+        , m_loadPasswordAction(nullptr)
+    {
+    }
+
+    PassStore* q_ptr;
+    QLineEdit* m_lineEdit;
+    QAction* m_loadPasswordAction;
+    QString m_applicationPrefix;
+    QString m_passwordId;
+
+    QString passwordFile()
+    {
+        return QStringLiteral("%1/.password-store/%2/%3.gpg").arg(QDir::home().absolutePath(), m_applicationPrefix, m_passwordId);
+    }
+};
+
+PassStore::PassStore(QLineEdit* parent, const QString& applicationPrefix, const QString& id)
+    : QObject(parent)
+    , d_ptr(new PassStorePrivate(this))
+{
+    Q_D(PassStore);
+    d->m_lineEdit = parent;
+    d->m_applicationPrefix = applicationPrefix;
+    d->m_loadPasswordAction = d->m_lineEdit->addAction(Icons::get(Icon::Vault), QLineEdit::TrailingPosition);
+    d->m_loadPasswordAction->setToolTip(i18n("Read the password from pass store"));
+
+    setPasswordId(id);
+
+    connect(d->m_loadPasswordAction, &QAction::triggered, this, [&]() {
+        Q_D(PassStore);
+        KGPGFile passwordFile;
+        passwordFile.setFileName(d->passwordFile());
+        if (passwordFile.open(QIODevice::ReadOnly)) {
+            QTextStream stream(&passwordFile);
+            const auto pwd = stream.readLine();
+            d->m_lineEdit->clear();
+            d->m_lineEdit->setText(pwd);
+        }
+    });
+}
+
+PassStore::~PassStore()
+{
+    Q_D(PassStore);
+    delete d;
+}
+
+void PassStore::setPasswordId(const QString& id)
+{
+    Q_D(PassStore);
+    d->m_passwordId = id;
+
+    // control visibility of icon
+    bool visible = false;
+    if (KGPGFile::GPGAvailable() && !id.isEmpty()) {
+        QFileInfo fi(d->passwordFile());
+        visible = (fi.exists() && fi.isReadable());
+    }
+    d->m_loadPasswordAction->setVisible(visible);
+}
+
+bool PassStore::isActionVisible() const
+{
+    Q_D(const PassStore);
+    return d->m_loadPasswordAction->isVisible();
+}
---
+++ kmymoney/widgets/passstore.h
@@ -0,0 +1,81 @@
+/*
+    SPDX-FileCopyrightText: 2022 Thomas Baumgart <tbaumgart@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef PASSSTORE_H
+#define PASSSTORE_H
+
+#include "kmm_base_widgets_export.h"
+
+// ----------------------------------------------------------------------------
+// QT Includes
+
+#include <QObject>
+#include <QString>
+
+// ----------------------------------------------------------------------------
+// KDE Includes
+
+// ----------------------------------------------------------------------------
+// Project Includes
+
+class QLineEdit;
+class QAction;
+
+class PassStorePrivate;
+/**
+ * This class implements a mechanism to enrich a QLineEdit with an action
+ * icon to load its content by extracting from a password store maintained
+ * by pass (see https://www.passwordstore.org). The path to the password
+ * file is identified by the @c applicationPrefix and @c id provided
+ * at construction time or the @c id passed with setPasswordId().
+ *
+ * If GPG is not available or a password file identified by
+ * @c applicationPrefix and @c id is not available or not
+ * readable, the icon to load the password is not visible
+ * inside the QLineEdit.
+ *
+ * The file for the password is expected to be in
+ * ~/.password-store/<applicationPrefix>/<id>.gpg
+ *
+ * To use this class, the following is needed (example provided for
+ * KMyMoney and KBanking plugin):
+ *
+ * @code
+ *
+ *    extern QString accountRef;
+ *    auto lineedit = new QLineEdit;
+ *    new PassStore(lineedit, QLatin1String("KMyMoney/KBanking"), accountRef);
+ *
+ * @endcode
+ *
+ * @note relies on kmm_gpgfile
+ *
+ * @author Thomas Baumgart
+ */
+class KMM_BASE_WIDGETS_EXPORT PassStore : public QObject
+{
+    Q_DECLARE_PRIVATE(PassStore);
+    Q_OBJECT
+public:
+    explicit PassStore(QLineEdit* parent, const QString& applicationPrefix, const QString& id = QString());
+    ~PassStore();
+
+    /**
+     * Update the password id to @a id. This will check for
+     * the password in the store and update the icon accordingly.
+     */
+    void setPasswordId(const QString& id);
+
+    /**
+     * Return if the action is visible or not. This can be used
+     * by code to check if a stored password is available or not.
+     */
+    bool isActionVisible() const;
+
+private:
+    PassStorePrivate* d_ptr;
+};
+
+#endif // PASSSTORE_H
