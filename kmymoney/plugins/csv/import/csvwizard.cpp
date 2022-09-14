KDE/kmymoney

commit be11d7f2aa38a7a83f2edab806227eeacfb3fba0

Port away from deprecated QDesktopWidget

kmymoney/plugins/csv/import/csvwizard.cpp
@@ -10,15 +10,16 @@
 // ----------------------------------------------------------------------------
 // QT Includes
 
-#include <QAction>
-#include <QDesktopWidget>
-#include <QTextCodec>
 #include <QAbstractButton>
+#include <QAction>
 #include <QFileDialog>
-#include <QStandardItemModel>
-#include <QScrollBar>
-#include <QLineEdit>
 #include <QKeyEvent>
+#include <QLineEdit>
+#include <QScreen>
+#include <QScrollBar>
+#include <QStandardItemModel>
+#include <QTextCodec>
+#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -220,8 +221,8 @@ void CSVWizard::updateWindowSize()
     table->resizeColumnsToContents();
     this->repaint();
 
-    QRect screen = QApplication::desktop()->availableGeometry();    //get available screen size
-    QRect wizard = this->frameGeometry();                           //get current wizard size
+    QSize screen = windowHandle()->screen()->size(); // get available screen size
+    QRect wizard = this->frameGeometry(); // get current wizard size
 
     int newWidth = (table->contentsMargins().left() //
                     + table->contentsMargins().right() //
@@ -490,14 +491,10 @@ void IntroPage::initializePage()
         m_dlg->m_initialWidth = m_dlg->geometry().width();
     } else {
         //resize wizard to its initial size and center it
-        m_dlg->setGeometry(
-            QStyle::alignedRect(
-                Qt::LeftToRight,
-                Qt::AlignCenter,
-                QSize(m_dlg->m_initialWidth, m_dlg->m_initialHeight),
-                QApplication::desktop()->availableGeometry()
-            )
-        );
+        m_dlg->setGeometry(QStyle::alignedRect(Qt::LeftToRight,
+                                               Qt::AlignCenter,
+                                               QSize(m_dlg->m_initialWidth, m_dlg->m_initialHeight),
+                                               windowHandle()->screen()->availableGeometry()));
     }
     m_dlg->ui->tableView->hide();
 }

kmymoney/plugins/csv/import/transactiondlg.cpp
@@ -9,8 +9,9 @@
 // ----------------------------------------------------------------------------
 // QT Headers
 
-#include <QDesktopWidget>
 #include <QPushButton>
+#include <QScreen>
+#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Headers
@@ -82,7 +83,7 @@ void TransactionDlg::updateWindowSize()
     int columnsWidth = 0;
     int headerWidth;
     int newWidth;
-    QRect screen = QApplication::desktop()->availableGeometry();
+    QSize screen = windowHandle()->screen()->size();
 
     layout()->invalidate();
     layout()->activate();

kmymoney/widgets/kmymoneycompletion.cpp
@@ -11,11 +11,12 @@
 // QT Includes
 
 #include <QApplication>
-#include <QKeyEvent>
 #include <QEvent>
-#include <QDesktopWidget>
+#include <QKeyEvent>
 #include <QLineEdit>
+#include <QScreen>
 #include <QVBoxLayout>
+#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -106,7 +107,7 @@ void KMyMoneyCompletion::adjustSize(const int count)
         // the code of this basic block is taken from KCompletionBox::show()
         // and modified to our local needs
 
-        QRect screenSize = QApplication::desktop()->availableGeometry(parentWidget());
+        QRect screenSize = windowHandle()->screen()->availableGeometry();
 
         QPoint orig = d->m_parent->mapToGlobal(QPoint(0, d->m_parent->height()));
         int x = orig.x();

kmymoney/widgets/popuppositioner.cpp
@@ -9,10 +9,10 @@
 // QT Includes
 
 #include <QApplication>
-#include <QDesktopWidget>
+#include <QRect>
 #include <QScreen>
 #include <QWidget>
-#include <QRect>
+#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -23,10 +23,7 @@
 
 PopupPositioner::PopupPositioner(QWidget* baseWidget, QWidget* popupWidget, Anchor anchor)
 {
-    // the following two lines use deprecated functions. I have not yet
-    // found out how to obtain the same information with newer versions (> 5.14)
-    auto screenNr = QApplication::desktop()->screenNumber(baseWidget);
-    const auto screenRect = QApplication::desktop()->screenGeometry(screenNr);
+    const auto screenRect = baseWidget->windowHandle()->screen()->availableGeometry();
 
     auto p = baseWidget->mapToGlobal(QPoint());
     // align the y coordinate

commit 64ba6353b7120e58f0fc1c846ef92c3908d46fad

Prevent crashes introduced with commit be11d7f2

kmymoney/plugins/csv/import/csvwizard.cpp
@@ -19,7 +19,6 @@
 #include <QScrollBar>
 #include <QStandardItemModel>
 #include <QTextCodec>
-#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -221,7 +220,7 @@ void CSVWizard::updateWindowSize()
     table->resizeColumnsToContents();
     this->repaint();
 
-    QSize screen = windowHandle()->screen()->size(); // get available screen size
+    QSize screen = this->screen()->size(); // get available screen size
     QRect wizard = this->frameGeometry(); // get current wizard size
 
     int newWidth = (table->contentsMargins().left() //
@@ -491,10 +490,8 @@ void IntroPage::initializePage()
         m_dlg->m_initialWidth = m_dlg->geometry().width();
     } else {
         //resize wizard to its initial size and center it
-        m_dlg->setGeometry(QStyle::alignedRect(Qt::LeftToRight,
-                                               Qt::AlignCenter,
-                                               QSize(m_dlg->m_initialWidth, m_dlg->m_initialHeight),
-                                               windowHandle()->screen()->availableGeometry()));
+        m_dlg->setGeometry(
+            QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, QSize(m_dlg->m_initialWidth, m_dlg->m_initialHeight), screen()->availableGeometry()));
     }
     m_dlg->ui->tableView->hide();
 }

kmymoney/plugins/csv/import/transactiondlg.cpp
@@ -11,7 +11,6 @@
 
 #include <QPushButton>
 #include <QScreen>
-#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Headers
@@ -83,7 +82,7 @@ void TransactionDlg::updateWindowSize()
     int columnsWidth = 0;
     int headerWidth;
     int newWidth;
-    QSize screen = windowHandle()->screen()->size();
+    QSize screen = this->screen()->size();
 
     layout()->invalidate();
     layout()->activate();

kmymoney/widgets/kmymoneycompletion.cpp
@@ -16,7 +16,6 @@
 #include <QLineEdit>
 #include <QScreen>
 #include <QVBoxLayout>
-#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -107,7 +106,7 @@ void KMyMoneyCompletion::adjustSize(const int count)
         // the code of this basic block is taken from KCompletionBox::show()
         // and modified to our local needs
 
-        QRect screenSize = windowHandle()->screen()->availableGeometry();
+        QRect screenSize = screen()->availableGeometry();
 
         QPoint orig = d->m_parent->mapToGlobal(QPoint(0, d->m_parent->height()));
         int x = orig.x();
@@ -182,7 +181,7 @@ bool KMyMoneyCompletion::eventFilter(QObject* o, QEvent* e)
     KMyMoneyCombo *c = dynamic_cast<KMyMoneyCombo*>(d->m_parent);
     if (o == d->m_parent || (c && o == c->lineEdit()) || o == this) {
         if (isVisible()) {
-#ifdef Q_OS_WIN32                   //krazy:exclude=cpp 
+#ifdef Q_OS_WIN32 // krazy:exclude=cpp
             // hide the completer only if the focus was not lost because of windows activation or the activated window is not an application window
             if (e->type() == QEvent::FocusOut && (static_cast<QFocusEvent*>(e)->reason() != Qt::ActiveWindowFocusReason || QApplication::activeWindow() == 0)) {
 #else

kmymoney/widgets/popuppositioner.cpp
@@ -12,7 +12,6 @@
 #include <QRect>
 #include <QScreen>
 #include <QWidget>
-#include <QWindow>
 
 // ----------------------------------------------------------------------------
 // KDE Includes
@@ -23,7 +22,7 @@
 
 PopupPositioner::PopupPositioner(QWidget* baseWidget, QWidget* popupWidget, Anchor anchor)
 {
-    const auto screenRect = baseWidget->windowHandle()->screen()->availableGeometry();
+    const auto screenRect = baseWidget->screen()->availableGeometry();
 
     auto p = baseWidget->mapToGlobal(QPoint());
     // align the y coordinate