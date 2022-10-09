59f1d10ecfcc1614abbc37883bb78cd36a8717a2
Dawid Wróbel <me@dawidwrobel.com>
2022-10-09 21:10:10 +0200

    Also accommodate Qt6 build of QtKeychain

--- kmymoney/misc/kmmkeychain.h
+++ kmymoney/misc/kmmkeychain.h
@@ -9,7 +9,11 @@
 
 #include <QObject>
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 #include <qt5keychain/keychain.h>
+#else
+#include <qt6keychain/keychain.h>
+#endif
 
 #include "kmm_keychain_export.h"
 
