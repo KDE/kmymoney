cc213a5e2874fb9d3e93e0af917b53e482c67a11
Thomas Baumgart <thb@net-bembel.de>
2022-11-06 09:47:59 +0100

    Improve CSV importer's date handling
    See https://forum.kde.org/viewtopic.php?f=69&t=174724 for description of
    the initial problem.

--- kmymoney/plugins/csv/import/core/convdate.cpp
+++ kmymoney/plugins/csv/import/core/convdate.cpp
@@ -6,6 +6,7 @@
 
 #include "convdate.h"
 
+#include <QDebug>
 #include <QLocale>
 #include <QRegularExpression>
 
@@ -37,6 +38,16 @@ QDate ConvertDate::convertDate(const QString& txt)
 
     QRegularExpression rx(QStringLiteral("[\\. :-]"));  //                           replace date field separators '.' ' ' ':' '-'
     QString buffer = txt.trimmed();
+
+    // in case we detect more than 3 sections, it could be,
+    // that a single dot is appended to a monthname. We
+    // remove this single dot before we continue
+    if (buffer.count(rx) > 2) {
+        if (buffer.count(QLatin1Char('.')) == 1) {
+            buffer.remove(QLatin1Char('.'));
+        }
+    }
+
     buffer = buffer.replace(rx, QString::fromLatin1("/"));   //     ....with '/'
     int count = buffer.count(QLatin1Char('/'), Qt::CaseSensitive);
     if (count == 0) {      //                              no separators so use QDate()
@@ -138,28 +149,26 @@ QDate ConvertDate::convertDate(const QString& txt)
     } else {                             //                  not a valid date
         return QDate();
     }
-//                                                 Check year
+
+    // Check year
     if (aYear.length() == 2) {       //                    2 digits
         if ((aYear.toInt() >= 0) && (aYear.toInt() < 50)) {
             aYear.prepend(QLatin1String("20"));//                      take year to be 2000-2049
         } else if ((aYear.toInt() >= 50) && (aYear.toInt() <= 99))
             aYear.prepend(QLatin1String("19"));//                      take year to be 1950-1999
-    } else if (aYear.length() == 4) {
-        if ((aYear.toInt() < 1950) || (aYear.toInt() > 2050)) {      //  not a valid year
-            return QDate();
-        }
-    } else {
+    } else if (aYear.length() != 4) {
         return QDate();//                              2 or 4 digits for a valid year
     }
-    // only years 1950-2050 valid
-    //                                               check day
+
+    // check day
     if (aDay.length() == 1)
         aDay.prepend(QLatin1Char('0'));//                           add a leading '0'
     if ((aDay.toInt() < 0) || (aDay.toInt() > 31)      //              check day value
             || (aDay.length()  < 1) || (aDay.length()  > 2)) {
         return QDate();//                              not a valid day
     }
-//                                                 check month
+
+    // check month
     if (aMonth.length() == 1) {
         aMonth.prepend(QLatin1Char('0'));
         aFormat = QLatin1String("MM");
@@ -173,37 +182,45 @@ QDate ConvertDate::convertDate(const QString& txt)
         int i;
         if (aMonth.length() > 3) {
             for (i = 1; i <= 12; ++i)
-                if (aMonth.compare(QDate::longMonthName(i, QDate::StandaloneFormat), Qt::CaseInsensitive) == 0)
+                if (aMonth.compare(QLocale().standaloneMonthName(i, QLocale::LongFormat), Qt::CaseInsensitive) == 0)
                     break;
             if (i == 13) {
                 for (i = 1; i <= 12; ++i)
-                    if (aMonth.compare(QDate::longMonthName(i), Qt::CaseInsensitive) == 0)
+                    if (aMonth.compare(QLocale().monthName(i, QLocale::LongFormat), Qt::CaseInsensitive) == 0)
                         break;
             }
         } else {
             for (i = 1; i <= 12; ++i)
-                if (aMonth.compare(QDate::shortMonthName(i, QDate::StandaloneFormat), Qt::CaseInsensitive) == 0)
+                if (aMonth.compare(QLocale().standaloneMonthName(i, QLocale::ShortFormat).remove(QLatin1String(".")), Qt::CaseInsensitive) == 0)
                     break;
             if (i == 13) {
                 for (i = 1; i <= 12; ++i)
-                    if (aMonth.compare(QDate::shortMonthName(i), Qt::CaseInsensitive) == 0)
+                    if (aMonth.compare(QLocale().monthName(i, QLocale::ShortFormat), Qt::CaseInsensitive) == 0)
                         break;
             }
         }
 
-        if (i == 13)
+        if (i == 13) {
+            QLocale locale;
+            qDebug() << "convertDate: Unable to find month" << aMonth << "in";
+            qDebug() << locale.name() << locale.bcp47Name() << locale.language() << locale.country() << locale.script();
+            for (i = 1; i <= 12; ++i) {
+                qDebug() << i << locale.standaloneMonthName(i, QLocale::ShortFormat) << locale.monthName(i, QLocale::ShortFormat)
+                         << locale.standaloneMonthName(i, QLocale::LongFormat);
+            }
             return QDate();
+        }
         aMonth = QString::fromLatin1("%1").arg(i, 2, 10, QLatin1Char('0'));
         aFormat = QLatin1String("MM");
     }
 
     QString dateFormat;
     // deal with Feb 30th
-    if ((aMonth == QLatin1Literal("02")) && (aDay == QLatin1Literal("30"))) {
+    if ((aMonth == QLatin1String("02")) && (aDay == QLatin1String("30"))) {
         if (QDate(aYear.toUInt(), 2, 29).isValid()) {
-            aDay = QLatin1Literal("29");
+            aDay = QLatin1String("29");
         } else {
-            aDay = QLatin1Literal("28");
+            aDay = QLatin1String("28");
         }
     }
     switch (m_dateFormatIndex) {
