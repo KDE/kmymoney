e549fcfcc3f9f80416a3b6ffd34fe915d019e0b3
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-02-10 20:18:10 +0100

    Clean up preparetips leftover in Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -2,9 +2,6 @@
 # invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
 # the results are stored in a pseudo .cpp file to be picked up by xgettext.
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-# if your application contains tips-of-the-day, call preparetips as well.
-cd kmymoney/ && $PREPARETIPS > tips.cpp
-cd ..
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cpp, and .h, just add them in the find call.
 $XGETTEXT `find . -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kmymoney.pot
