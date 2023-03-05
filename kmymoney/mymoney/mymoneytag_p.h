5a780bdce7b03d2a6f66e1646550f25151661027
Thomas Baumgart <thb@net-bembel.de>
2023-03-05 12:05:21 +0100

    Resolve compiler warning
    base class ‘class MyMoneyObjectPrivate’ should be explicitly initialized
    in the copy constructor

--- kmymoney/mymoney/mymoneytag_p.h
+++ kmymoney/mymoney/mymoneytag_p.h
@@ -22,18 +22,18 @@
 class MyMoneyTagPrivate : public MyMoneyObjectPrivate
 {
 public:
-
-    MyMoneyTagPrivate() :
-        m_closed(false),
-        m_tag_color(QColor("black"))
+    MyMoneyTagPrivate()
+        : m_closed(false)
+        , m_tag_color(QColor("black"))
     {
     }
 
-    MyMoneyTagPrivate(const MyMoneyTagPrivate& d) :
-        m_name(d.m_name),
-        m_closed(d.m_closed),
-        m_tag_color(d.m_tag_color),
-        m_notes(d.m_notes)
+    MyMoneyTagPrivate(const MyMoneyTagPrivate& d)
+        : MyMoneyObjectPrivate(d)
+        , m_name(d.m_name)
+        , m_closed(d.m_closed)
+        , m_tag_color(d.m_tag_color)
+        , m_notes(d.m_notes)
     {
     }
 
