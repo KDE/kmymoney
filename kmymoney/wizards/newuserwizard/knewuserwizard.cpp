c29012e25bd265711e71c40245125439fd4eebde
Thomas Baumgart <thb@net-bembel.de>
2024-01-02 10:32:30 +0100

    Fix link to documentation
    BUG: 479248
    FIXED-IN: 5.2

--- kmymoney/wizards/newuserwizard/knewuserwizard.cpp
+++ kmymoney/wizards/newuserwizard/knewuserwizard.cpp
@@ -73,7 +73,7 @@ Wizard::Wizard(QWidget *parent, bool modal, Qt::WindowFlags flags) :
     else
         d->setFirstPage(d->m_generalPage);
 
-    setHelpContext("firsttime-3");
+    setHelpContext("firsttime");
 }
 
 Wizard::~Wizard()
