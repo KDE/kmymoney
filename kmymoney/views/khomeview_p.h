KDE/kmymoney

commit cd941ab45329b1c0484bdd683495f204d99a1bf5

Combine duplicated code copies

kmymoney/views/khomeview_p.h
@@ -597,6 +597,34 @@ public:
         m_adjustedIconSize = isize;
     }
 
+    void addScheduleHeader(const QString& title, const QString& cssClass)
+    {
+        m_html += QString("<tr class=\"%1\"><td colspan=\"7\">%2</td></tr>\n").arg(cssClass, title);
+        m_html += "<tr class=\"item\">";
+        m_html += "<td class=\"left\" width=\"10%\">";
+        m_html += i18n("Date");
+        m_html += "</td>";
+        m_html += "<td class=\"left\" width=\"2%\">";
+        m_html += i18n("Next");
+        m_html += "</td>";
+        m_html += "<td class=\"left\" width=\"2%\">";
+        m_html += i18n("Skip");
+        m_html += "</td>";
+        m_html += "<td class=\"left\" width=\"36%\">";
+        m_html += i18n("Schedule");
+        m_html += "</td>";
+        m_html += "<td class=\"left\" width=\"20%\">";
+        m_html += i18n("Account");
+        m_html += "</td>";
+        m_html += "<td class=\"right nowrap\" width=\"15%\">";
+        m_html += i18n("Amount");
+        m_html += "</td>";
+        m_html += "<td class=\"right nowrap\" width=\"15%\">";
+        m_html += i18n("Balance after");
+        m_html += "</td>";
+        m_html += "</tr>";
+    }
+
     void showScheduledPayments()
     {
         MyMoneyFile* file = MyMoneyFile::instance();
@@ -656,30 +684,7 @@ public:
             QList<MyMoneySchedule>::Iterator it_f;
 
             m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-            m_html += QString("<tr class=\"itemtitle negativetext\"><td colspan=\"7\">%1</td></tr>\n").arg(i18n("Overdue payments"));
-            m_html += "<tr class=\"item\">";
-            m_html += "<td class=\"left\" width=\"10%\">";
-            m_html += i18n("Date");
-            m_html += "</td>";
-            m_html += "<td class=\"left\" width=\"2%\">";
-            m_html += i18n("Next");
-            m_html += "</td>";
-            m_html += "<td class=\"left\" width=\"2%\">";
-            m_html += i18n("Skip");
-            m_html += "</td>";
-            m_html += "<td class=\"left\" width=\"36%\">";
-            m_html += i18n("Schedule");
-            m_html += "</td>";
-            m_html += "<td class=\"left\" width=\"20%\">";
-            m_html += i18n("Account");
-            m_html += "</td>";
-            m_html += "<td class=\"right nowrap\" width=\"15%\">";
-            m_html += i18n("Amount");
-            m_html += "</td>";
-            m_html += "<td class=\"right nowrap\" width=\"15%\">";
-            m_html += i18n("Balance after");
-            m_html += "</td>";
-            m_html += "</tr>";
+            addScheduleHeader(QLatin1String("itemtitle negativetext"), i18n("Overdue payments"));
 
             int i = 0;
             for (it = overdues.begin(); it != overdues.end(); ++it) {
@@ -716,30 +721,7 @@ public:
             if (todays.count() > 0) {
                 m_html += "<tr class=\"gap\"><td>&nbsp;\n</td></tr>";
                 m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-                m_html += QString("<tr class=\"itemtitle\"><td class=\"left\" colspan=\"7\">%1</td></tr>\n").arg(i18n("Today's due payments"));
-                m_html += "<tr class=\"item\">";
-                m_html += "<td class=\"left\" width=\"10%\">";
-                m_html += i18n("Date");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"2%\">";
-                m_html += i18n("Next");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"2%\">";
-                m_html += i18n("Skip");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"36%\">";
-                m_html += i18n("Schedule");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"20%\">";
-                m_html += i18n("Account");
-                m_html += "</td>";
-                m_html += "<td class=\"right nowrap\" width=\"15%\">";
-                m_html += i18n("Amount");
-                m_html += "</td>";
-                m_html += "<td class=\"right nowrap\" width=\"15%\">";
-                m_html += i18n("Balance after");
-                m_html += "</td>";
-                m_html += "</tr>";
+                addScheduleHeader(QLatin1String("itemtitle"), i18n("Today's due payments"));
 
                 int i = 0;
                 for (t_it = todays.begin(); t_it != todays.end(); ++t_it) {
@@ -754,30 +736,7 @@ public:
                 QList<MyMoneySchedule>::Iterator it;
 
                 m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-                m_html += QString("<tr class=\"itemtitle\"><td class=\"left\" colspan=\"7\">%1</td></tr>\n").arg(i18n("Future payments"));
-                m_html += "<tr class=\"item\">";
-                m_html += "<td class=\"left\" width=\"10%\">";
-                m_html += i18n("Date");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"2%\">";
-                m_html += i18n("Next");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"2%\">";
-                m_html += i18n("Skip");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"36%\">";
-                m_html += i18n("Schedule");
-                m_html += "</td>";
-                m_html += "<td class=\"left\" width=\"20%\">";
-                m_html += i18n("Account");
-                m_html += "</td>";
-                m_html += "<td class=\"right nowrap\" width=\"15%\">";
-                m_html += i18n("Amount");
-                m_html += "</td>";
-                m_html += "<td class=\"right nowrap\" width=\"15%\">";
-                m_html += i18n("Balance after");
-                m_html += "</td>";
-                m_html += "</tr>";
+                addScheduleHeader(QLatin1String("itemtitle"), i18n("Future payments"));
 
                 // show all or the first 6 entries
                 int cnt;

kmymoney/kmymoney.cpp
@@ -279,11 +279,6 @@ public:
         m_actionCollectorTimer.setInterval(100);
     }
 
-    void unlinkStatementXML();
-    void moveInvestmentTransaction(const QString& fromId,
-                                   const QString& toId,
-                                   const MyMoneyTransaction& t);
-
     struct storageInfo {
         eKMyMoney::StorageType type {eKMyMoney::StorageType::None};
         bool isOpened {false};
@@ -1427,6 +1422,75 @@ public:
             return LedgerViewSettings::ShowLastReconciliationHeader;
         default:
             return LedgerViewSettings::ShowAllReconciliationHeader;
+        }
+    }
+
+    /**
+     * Create a new stock account as a copy of @a sourceStockAccountId in
+     * the invesment account identified by @a parentInvestAccount.
+     */
+    QString createNewStockAccount(MyMoneyAccount& parentInvestAccount, const QString& sourceStockAccountId)
+    {
+        MyMoneyAccount stockAccount = MyMoneyFile::instance()->account(sourceStockAccountId);
+        MyMoneyAccount newStock;
+        newStock.setName(stockAccount.name());
+        newStock.setNumber(stockAccount.number());
+        newStock.setDescription(stockAccount.description());
+        newStock.setInstitutionId(stockAccount.institutionId());
+        newStock.setOpeningDate(stockAccount.openingDate());
+        newStock.setAccountType(stockAccount.accountType());
+        newStock.setCurrencyId(stockAccount.currencyId());
+        newStock.setClosed(stockAccount.isClosed());
+        MyMoneyFile::instance()->addAccount(newStock, parentInvestAccount);
+        return newStock.id();
+    }
+
+    // move a stock transaction from one investment account to another
+    void moveInvestmentTransaction(const QString& /*fromId*/, const QString& toId, const MyMoneyTransaction& tx)
+    {
+        MyMoneyAccount toInvAcc = MyMoneyFile::instance()->account(toId);
+        MyMoneyTransaction t(tx);
+        // first determine which stock we are dealing with.
+        // fortunately, investment transactions have only one stock involved
+        QString stockAccountId;
+        QString stockSecurityId;
+        MyMoneySplit s;
+        const auto splits = t.splits();
+        for (const auto& split : splits) {
+            stockAccountId = split.accountId();
+            stockSecurityId = MyMoneyFile::instance()->account(stockAccountId).currencyId();
+            if (!MyMoneyFile::instance()->security(stockSecurityId).isCurrency()) {
+                s = split;
+                break;
+            }
+        }
+        // Now check the target investment account to see if it
+        // contains a stock with this id
+        QString newStockAccountId;
+        const auto accountList = toInvAcc.accountList();
+        for (const auto& sAccount : accountList) {
+            if (MyMoneyFile::instance()->account(sAccount).currencyId() == stockSecurityId) {
+                newStockAccountId = sAccount;
+                break;
+            }
+        }
+        // if it doesn't exist, we need to add it as a copy of the old one
+        // no 'copyAccount()' function??
+        if (newStockAccountId.isEmpty()) {
+            newStockAccountId = createNewStockAccount(toInvAcc, stockAccountId);
+        }
+        // now update the split and the transaction
+        s.setAccountId(newStockAccountId);
+        t.modifySplit(s);
+        MyMoneyFile::instance()->modifyTransaction(t);
+    }
+
+    void unlinkStatementXML()
+    {
+        QDir dir(KMyMoneySettings::logPath(), "kmm-statement*");
+        for (uint i = 0; i < dir.count(); ++i) {
+            qDebug("Remove %s", qPrintable(dir[i]));
+            dir.remove(KMyMoneySettings::logPath() + QString("/%1").arg(dir[i]));
         }
     }
 };
@@ -3016,18 +3080,7 @@ void KMyMoneyApp::slotMoveTransactionTo()
                     // if it doesn't exist, we need to add it as a copy of the old one
                     // no 'copyAccount()' function??
                     if (newStockAccountId.isEmpty()) {
-                        MyMoneyAccount stockAccount = file->account(stockAccountId);
-                        MyMoneyAccount newStock;
-                        newStock.setName(stockAccount.name());
-                        newStock.setNumber(stockAccount.number());
-                        newStock.setDescription(stockAccount.description());
-                        newStock.setInstitutionId(stockAccount.institutionId());
-                        newStock.setOpeningDate(stockAccount.openingDate());
-                        newStock.setAccountType(stockAccount.accountType());
-                        newStock.setCurrencyId(stockAccount.currencyId());
-                        newStock.setClosed(stockAccount.isClosed());
-                        file->addAccount(newStock, toInvAcc);
-                        newStockAccountId = newStock.id();
+                        newStockAccountId = d->createNewStockAccount(toInvAcc, stockAccountId);
                     }
 
                     // now update the split and the transaction
@@ -4030,62 +4083,6 @@ void KMyMoneyApp::slotNewFeature()
 {
 }
 
-// move a stock transaction from one investment account to another
-void KMyMoneyApp::Private::moveInvestmentTransaction(const QString& /*fromId*/,
-        const QString& toId,
-        const MyMoneyTransaction& tx)
-{
-    MyMoneyAccount toInvAcc = MyMoneyFile::instance()->account(toId);
-    MyMoneyTransaction t(tx);
-    // first determine which stock we are dealing with.
-    // fortunately, investment transactions have only one stock involved
-    QString stockAccountId;
-    QString stockSecurityId;
-    MyMoneySplit s;
-    const auto splits = t.splits();
-    for (const auto& split : splits) {
-        stockAccountId = split.accountId();
-        stockSecurityId =
-            MyMoneyFile::instance()->account(stockAccountId).currencyId();
-        if (!MyMoneyFile::instance()->security(stockSecurityId).isCurrency()) {
-            s = split;
-            break;
-        }
-    }
-    // Now check the target investment account to see if it
-    // contains a stock with this id
-    QString newStockAccountId;
-    const auto accountList = toInvAcc.accountList();
-    for (const auto& sAccount : accountList) {
-        if (MyMoneyFile::instance()->account(sAccount).currencyId() ==
-                stockSecurityId) {
-            newStockAccountId = sAccount;
-            break;
-        }
-    }
-    // if it doesn't exist, we need to add it as a copy of the old one
-    // no 'copyAccount()' function??
-    if (newStockAccountId.isEmpty()) {
-        MyMoneyAccount stockAccount =
-            MyMoneyFile::instance()->account(stockAccountId);
-        MyMoneyAccount newStock;
-        newStock.setName(stockAccount.name());
-        newStock.setNumber(stockAccount.number());
-        newStock.setDescription(stockAccount.description());
-        newStock.setInstitutionId(stockAccount.institutionId());
-        newStock.setOpeningDate(stockAccount.openingDate());
-        newStock.setAccountType(stockAccount.accountType());
-        newStock.setCurrencyId(stockAccount.currencyId());
-        newStock.setClosed(stockAccount.isClosed());
-        MyMoneyFile::instance()->addAccount(newStock, toInvAcc);
-        newStockAccountId = newStock.id();
-    }
-    // now update the split and the transaction
-    s.setAccountId(newStockAccountId);
-    t.modifySplit(s);
-    MyMoneyFile::instance()->modifyTransaction(t);
-}
-
 void KMyMoneyApp::slotDataChanged()
 {
     d->fileAction(eKMyMoney::FileAction::Changed);
@@ -4901,15 +4898,6 @@ KMStatus::KMStatus(const QString &text)
 KMStatus::~KMStatus()
 {
     kmymoney->slotStatusMsg(m_prevText);
-}
-
-void KMyMoneyApp::Private::unlinkStatementXML()
-{
-    QDir dir(KMyMoneySettings::logPath(), "kmm-statement*");
-    for (uint i = 0; i < dir.count(); ++i) {
-        qDebug("Remove %s", qPrintable(dir[i]));
-        dir.remove(KMyMoneySettings::logPath() + QString("/%1").arg(dir[i]));
-    }
 }
 
 #include "kmymoney.moc"

kmymoney/mymoney/splitmodel.cpp
@@ -284,6 +284,24 @@ struct SplitModel::Private
         return value;
     }
 
+    void setupSplitData(MyMoneySplit& s, const QModelIndex& idx) const
+    {
+        s.setNumber(idx.data(eMyMoney::Model::SplitNumberRole).toString());
+        s.setMemo(idx.data(eMyMoney::Model::SplitMemoRole).toString());
+        s.setAccountId(idx.data(eMyMoney::Model::SplitAccountIdRole).toString());
+        s.setShares(idx.data(eMyMoney::Model::SplitSharesRole).value<MyMoneyMoney>());
+        s.setValue(idx.data(eMyMoney::Model::SplitValueRole).value<MyMoneyMoney>());
+        s.setCostCenterId(idx.data(eMyMoney::Model::SplitCostCenterIdRole).toString());
+        s.setPayeeId(idx.data(eMyMoney::Model::SplitPayeeIdRole).toString());
+        s.setTagIdList(idx.data(eMyMoney::Model::SplitTagIdRole).toStringList());
+
+        // update the price information. setting the price to zero
+        // will cause MyMoneySplit::price to recalculate the price
+        // based on value and shares of the split
+        s.setPrice(MyMoneyMoney());
+        s.setPrice(s.price());
+    }
+
     SplitModel* q;
     QHash<Column, QString> headerData;
     QString transactionCommodity;
@@ -639,21 +657,7 @@ void SplitModel::addSplitsToTransaction(MyMoneyTransaction& t) const
             s = t.splitById(splitId);
         } catch (const MyMoneyException&) {
         }
-        s.setNumber(idx.data(eMyMoney::Model::SplitNumberRole).toString());
-        s.setMemo(idx.data(eMyMoney::Model::SplitMemoRole).toString());
-        s.setAccountId(idx.data(eMyMoney::Model::SplitAccountIdRole).toString());
-        s.setShares(idx.data(eMyMoney::Model::SplitSharesRole).value<MyMoneyMoney>());
-        s.setValue(idx.data(eMyMoney::Model::SplitValueRole).value<MyMoneyMoney>());
-        s.setCostCenterId(idx.data(eMyMoney::Model::SplitCostCenterIdRole).toString());
-        s.setPayeeId(idx.data(eMyMoney::Model::SplitPayeeIdRole).toString());
-        s.setTagIdList(idx.data(eMyMoney::Model::SplitTagIdRole).toStringList());
-
-        // update the price information. setting the price to zero
-        // will cause MyMoneySplit::price to recalculate the price
-        // based on value and shares of the split
-        s.setPrice(MyMoneyMoney());
-        s.setPrice(s.price());
-
+        d->setupSplitData(s, idx);
         if (s.id().isEmpty()) {
             t.addSplit(s);
         } else {
@@ -669,21 +673,7 @@ QList<MyMoneySplit> SplitModel::splitList() const
     for (int row = 0; row < rows; ++row) {
         const auto idx = index(row, 0);
         MyMoneySplit s;
-        s.setNumber(idx.data(eMyMoney::Model::SplitNumberRole).toString());
-        s.setMemo(idx.data(eMyMoney::Model::SplitMemoRole).toString());
-        s.setAccountId(idx.data(eMyMoney::Model::SplitAccountIdRole).toString());
-        s.setShares(idx.data(eMyMoney::Model::SplitSharesRole).value<MyMoneyMoney>());
-        s.setValue(idx.data(eMyMoney::Model::SplitValueRole).value<MyMoneyMoney>());
-        s.setCostCenterId(idx.data(eMyMoney::Model::SplitCostCenterIdRole).toString());
-        s.setPayeeId(idx.data(eMyMoney::Model::SplitPayeeIdRole).toString());
-        s.setTagIdList(idx.data(eMyMoney::Model::SplitTagIdRole).toStringList());
-
-        // update the price information. setting the price to zero
-        // will cause MyMoneySplit::price to recalculate the price
-        // based on value and shares of the split
-        s.setPrice(MyMoneyMoney());
-        s.setPrice(s.price());
-
+        d->setupSplitData(s, idx);
         splits.append(s);
     }
     return splits;

kmymoney/mymoney/storage/journalmodel.cpp
@@ -39,6 +39,11 @@ struct JournalModel::Private
     } category_t;
 
     typedef enum {
+        Add,
+        Remove,
+    } BalanceCacheDirection;
+
+    typedef enum {
         StockSplitForward,
         StockSplitBackward,
     } StockSplitDirection;
@@ -308,32 +313,29 @@ struct JournalModel::Private
         fullBalanceRecalc.clear();
     }
 
-    void removeTransactionFromBalance(int startRow, int rows)
+    void updateTransactionFromBalance(int startRow, int rows, BalanceCacheDirection direction)
     {
         for (int row = 0; row < rows; ++row)  {
             const auto journalEntry = static_cast<TreeItem<JournalEntry>*>(q->index(startRow, 0).internalPointer())->constDataRef();
             balanceChangedSet.insert(journalEntry.split().accountId());
             if (Q_UNLIKELY(journalEntry.transaction().isStockSplit())) {
                 fullBalanceRecalc.insert(journalEntry.split().accountId());
+            } else if (direction == BalanceCacheDirection::Add) {
+                balanceCache[journalEntry.split().accountId()] += journalEntry.split();
             } else {
                 balanceCache[journalEntry.split().accountId()] -= journalEntry.split();
             }
             ++startRow;
         }
     }
+    void removeTransactionFromBalance(int startRow, int rows)
+    {
+        updateTransactionFromBalance(startRow, rows, BalanceCacheDirection::Remove);
+    }
 
     void addTransactionToBalance(int startRow, int rows)
     {
-        for (int row = 0; row < rows; ++row)  {
-            const auto journalEntry = static_cast<TreeItem<JournalEntry>*>(q->index(startRow, 0).internalPointer())->constDataRef();
-            balanceChangedSet.insert(journalEntry.split().accountId());
-            if (Q_UNLIKELY(journalEntry.transaction().isStockSplit())) {
-                fullBalanceRecalc.insert(journalEntry.split().accountId());
-            } else {
-                balanceCache[journalEntry.split().accountId()] += journalEntry.split();
-            }
-            ++startRow;
-        }
+        updateTransactionFromBalance(startRow, rows, BalanceCacheDirection::Add);
     }
 
     /**

kmymoney/views/ledgerview.cpp
@@ -283,28 +283,8 @@ public:
                 const auto selectedAccountId = selection.firstSelection(SelectedObjects::Account);
                 const auto accountIdx = file->accountsModel()->indexById(selectedAccountId);
                 AccountSet accountSet;
-                bool isInvestmentMove(false);
-                if (accountIdx.isValid()) {
-                    if (accountIdx.data(eMyMoney::Model::AccountTypeRole).value<eMyMoney::Account::Type>() == eMyMoney::Account::Type::Investment) {
-                        isInvestmentMove = true;
-                        accountSet.addAccountType(eMyMoney::Account::Type::Investment);
-                    } else if (accountIdx.data(eMyMoney::Model::AccountIsAssetLiabilityRole).toBool()) {
-                        accountSet.addAccountType(eMyMoney::Account::Type::Checkings);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Savings);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Cash);
-                        accountSet.addAccountType(eMyMoney::Account::Type::AssetLoan);
-                        accountSet.addAccountType(eMyMoney::Account::Type::CertificateDep);
-                        accountSet.addAccountType(eMyMoney::Account::Type::MoneyMarket);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Asset);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Currency);
-                        accountSet.addAccountType(eMyMoney::Account::Type::CreditCard);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Loan);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Liability);
-                    } else if (accountIdx.data(eMyMoney::Model::AccountIsIncomeExpenseRole).toBool()) {
-                        accountSet.addAccountType(eMyMoney::Account::Type::Income);
-                        accountSet.addAccountType(eMyMoney::Account::Type::Expense);
-                    }
-                } else {
+
+                auto loadAssetLiabilityTypes = [&]() {
                     accountSet.addAccountType(eMyMoney::Account::Type::Checkings);
                     accountSet.addAccountType(eMyMoney::Account::Type::Savings);
                     accountSet.addAccountType(eMyMoney::Account::Type::Cash);
@@ -316,6 +296,23 @@ public:
                     accountSet.addAccountType(eMyMoney::Account::Type::CreditCard);
                     accountSet.addAccountType(eMyMoney::Account::Type::Loan);
                     accountSet.addAccountType(eMyMoney::Account::Type::Liability);
+                };
+
+                bool isInvestmentMove(false);
+                if (accountIdx.isValid()) {
+                    if (accountIdx.data(eMyMoney::Model::AccountTypeRole).value<eMyMoney::Account::Type>() == eMyMoney::Account::Type::Investment) {
+                        isInvestmentMove = true;
+                        accountSet.addAccountType(eMyMoney::Account::Type::Investment);
+
+                    } else if (accountIdx.data(eMyMoney::Model::AccountIsAssetLiabilityRole).toBool()) {
+                        loadAssetLiabilityTypes();
+
+                    } else if (accountIdx.data(eMyMoney::Model::AccountIsIncomeExpenseRole).toBool()) {
+                        accountSet.addAccountType(eMyMoney::Account::Type::Income);
+                        accountSet.addAccountType(eMyMoney::Account::Type::Expense);
+                    }
+                } else {
+                    loadAssetLiabilityTypes();
                 }
 
                 accountSet.load(moveToAccountSelector);

kmymoney/widgets/kmymoneyselector.cpp
@@ -20,7 +20,6 @@
 // Project Includes
 
 #include "kmymoneysettings.h"
-#include "widgetenums.h"
 
 using namespace eWidgets;
 
@@ -159,17 +158,7 @@ QTreeWidgetItem* KMyMoneySelector::newTopItem(const QString& name, const QString
 {
     Q_D(KMyMoneySelector);
     QTreeWidgetItem* item = new QTreeWidgetItem(d->m_treeWidget);
-
-    item->setText(0, name);
-    item->setData(0, (int)Selector::Role::Key, key);
-    item->setData(0, (int)Selector::Role::Id, id);
-    item->setText(1, key); // hidden, but used for sorting
-    item->setFlags(item->flags() & ~Qt::ItemIsUserCheckable);
-
-    if (d->m_selMode == QTreeWidget::MultiSelection) {
-        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
-        item->setCheckState(0, Qt::Checked);
-    }
+    d->setupItemData(item, name, key, id);
     return item;
 }
 
@@ -177,17 +166,7 @@ QTreeWidgetItem* KMyMoneySelector::newItem(QTreeWidgetItem* parent, const QStrin
 {
     Q_D(KMyMoneySelector);
     QTreeWidgetItem* item = new QTreeWidgetItem(parent);
-
-    item->setText(0, name);
-    item->setData(0, (int)Selector::Role::Key, key);
-    item->setData(0, (int)Selector::Role::Id, id);
-    item->setText(1, key); // hidden, but used for sorting
-    item->setFlags(item->flags() & ~Qt::ItemIsUserCheckable);
-
-    if (d->m_selMode == QTreeWidget::MultiSelection) {
-        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
-        item->setCheckState(0, Qt::Checked);
-    }
+    d->setupItemData(item, name, key, id);
     return item;
 }
 

kmymoney/widgets/kmymoneyselector_p.h
@@ -22,6 +22,8 @@
 // ----------------------------------------------------------------------------
 // Project Includes
 
+#include "widgetenums.h"
+
 class QHBoxLayout;
 
 class KMyMoneySelectorPrivate
@@ -68,6 +70,20 @@ public:
 
         q->connect(m_treeWidget, &QTreeWidget::itemPressed, q, &KMyMoneySelector::slotItemPressed);
         q->connect(m_treeWidget, &QTreeWidget::itemChanged, q, &KMyMoneySelector::stateChanged);
+    }
+
+    void setupItemData(QTreeWidgetItem* item, const QString& name, const QString& key, const QString& id)
+    {
+        item->setText(0, name);
+        item->setData(0, (int)eWidgets::Selector::Role::Key, key);
+        item->setData(0, (int)eWidgets::Selector::Role::Id, id);
+        item->setText(1, key); // hidden, but used for sorting
+        item->setFlags(item->flags() & ~Qt::ItemIsUserCheckable);
+
+        if (m_selMode == QTreeWidget::MultiSelection) {
+            item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+            item->setCheckState(0, Qt::Checked);
+        }
     }
 
     KMyMoneySelector          *q_ptr;

commit 2ef9edd118c6a93fcbfafbcc85aa857cf74ffc3d

Fix order of arguments to addScheduleHeader
This fixes a problem introduced with commit cd941ab45

kmymoney/views/khomeview_p.h
@@ -684,7 +684,7 @@ public:
             QList<MyMoneySchedule>::Iterator it_f;
 
             m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-            addScheduleHeader(QLatin1String("itemtitle negativetext"), i18n("Overdue payments"));
+            addScheduleHeader(i18n("Overdue payments"), QLatin1String("itemtitle negativetext"));
 
             int i = 0;
             for (it = overdues.begin(); it != overdues.end(); ++it) {
@@ -721,7 +721,7 @@ public:
             if (todays.count() > 0) {
                 m_html += "<tr class=\"gap\"><td>&nbsp;\n</td></tr>";
                 m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-                addScheduleHeader(QLatin1String("itemtitle"), i18n("Today's due payments"));
+                addScheduleHeader(i18n("Today's due payments"), QLatin1String("itemtitle"));
 
                 int i = 0;
                 for (t_it = todays.begin(); t_it != todays.end(); ++t_it) {
@@ -736,7 +736,7 @@ public:
                 QList<MyMoneySchedule>::Iterator it;
 
                 m_html += "<tr><td><table width=\"100%\" cellspacing=\"0\" cellpadding=\"2\" class=\"summarytable\" >";
-                addScheduleHeader(QLatin1String("itemtitle"), i18n("Future payments"));
+                addScheduleHeader(i18n("Future payments"), QLatin1String("itemtitle"));
 
                 // show all or the first 6 entries
                 int cnt;