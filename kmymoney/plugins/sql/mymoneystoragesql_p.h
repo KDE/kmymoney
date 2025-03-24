d18712f2a1be425eedbfca1c1f12bae5d9883755
Ralf Habacker <ralf.habacker@freenet.de>
2025-03-24 06:29:44 +0100

    Use default update process to create additional columns for kmyPayees
    After adding a value for the initVersion parameter to the constructor
    of the MyMoneyDbTextColumn class, the standard update pattern with
    alterTable() can be used.
    
    CCBUG:501686
    CCBUG:345550
    
    Fixup for commit 17659e328.

--- kmymoney/plugins/sql/mymoneystoragesql_p.h
+++ kmymoney/plugins/sql/mymoneystoragesql_p.h
@@ -2606,27 +2606,9 @@ public:
         case 1: // column exists, nothing to do
             break;
         case 0: // need update of kmmPayees
-            QSqlQuery query(*q);
-            if (!query.exec("ALTER TABLE kmmPayees ADD COLUMN "
-                            + MyMoneyDbTextColumn("idPattern", MyMoneyDbTextColumn::NORMAL, false, false, true).generateDDL(m_driver))) {
-                buildError(query, Q_FUNC_INFO, "Error adding kmmPayees.idPattern");
+            // add column idPattern and urlTemplate to kmmPayees. Simply redo the update for 12
+            if (!alterTable(m_db.m_tables["kmmPayees"], m_dbVersion))
                 return 1;
-            }
-        }
-
-        switch (haveColumnInTable(QLatin1String("kmmPayees"), QLatin1String("urlTemplate"))) {
-        case -1:
-            return 1;
-        case 1: // column exists, nothing to do
-            break;
-        case 0: // need update of kmmPayees
-            QSqlQuery query(*q);
-            if (!query.exec("ALTER TABLE kmmPayees ADD COLUMN "
-                            + MyMoneyDbTextColumn("urlTemplate", MyMoneyDbTextColumn::NORMAL, false, false, true).generateDDL(m_driver))) {
-                buildError(query, Q_FUNC_INFO, "Error adding kmmPayees.urlTemplate");
-                return 1;
-            }
-            break;
         }
         return 0;
     }
