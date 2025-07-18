ce53932f050f697a5efda282b41e51712db8d22c
Thomas Baumgart <thb@net-bembel.de>
2025-07-18 16:07:07 +0200

    Provide method to quickly reach the ledger from the investment view
    Older versions kept the investment and ledger view in sync. Since 5.2 now supports multiple ledgers this is not possible anymore. Using the new button one can quickly switch to the ledger view of the opened investment account.
    
    BUG: 507191
    FIXED-IN: 5.2.1

--- kmymoney/views/kinvestmentview.cpp
+++ kmymoney/views/kinvestmentview.cpp
@@ -89,6 +89,9 @@ public:
         m_needLoad = false;
         ui->setupUi(q);
 
+        // initially disabled
+        ui->m_ledgerButton->setEnabled(false);
+
         // Equities tab
         m_accountsProxyModel = new AccountNamesFilterProxyModel(q);
         m_accountsProxyModel->setObjectName("m_accountsProxyModel");
@@ -286,6 +289,8 @@ public:
 KInvestmentView::KInvestmentView(QWidget *parent) :
     KMyMoneyViewBase(*new KInvestmentViewPrivate(this), parent)
 {
+    Q_D(KInvestmentView);
+
     connect(pActions[eMenu::Action::NewInvestment], &QAction::triggered, this, &KInvestmentView::slotNewInvestment);
     connect(pActions[eMenu::Action::EditInvestment], &QAction::triggered, this, &KInvestmentView::slotEditInvestment);
     connect(pActions[eMenu::Action::DeleteInvestment], &QAction::triggered, this, &KInvestmentView::slotDeleteInvestment);
@@ -469,6 +474,7 @@ void KInvestmentView::showEvent(QShowEvent* event)
         connect(d->ui->m_accountComboBox, &KMyMoneyAccountCombo::accountSelected, this, [&](const QString& accId) {
             Q_D(KInvestmentView);
             d->loadAccount(accId);
+            d->ui->m_ledgerButton->setDisabled(accId.isEmpty());
         });
 
         connect(MyMoneyFile::instance()->accountsModel(), &AccountsModel::rowsInserted, this, [&]() {
@@ -480,6 +486,16 @@ void KInvestmentView::showEvent(QShowEvent* event)
             // kick the necessary update of the view
             if (d->m_equitiesProxyModel->rowCount() == 0) {
                 QMetaObject::invokeMethod(this, &KInvestmentView::refreshEquities, Qt::QueuedConnection);
+            }
+        });
+
+        connect(d->ui->m_ledgerButton, &QToolButton::clicked, this, [&]() {
+            Q_D(KInvestmentView);
+            const auto id = d->m_equitySelections.firstSelection(SelectedObjects::Account);
+            if (!id.isEmpty()) {
+                const auto gotoAccount = pActions[eMenu::Action::GoToAccount];
+                gotoAccount->setData(id);
+                gotoAccount->trigger();
             }
         });
 
