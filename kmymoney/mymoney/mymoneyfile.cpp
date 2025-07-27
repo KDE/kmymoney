ca4ab396821241f5680572df9075efb3b2ceabc0
Thomas Baumgart <thb@net-bembel.de>
2025-07-27 10:10:13 +0200

    Revert "Debug output to analyze bug 507404"
    This reverts commit a519fa511af0fe37c19d504f4c30d7d53c63380d.

--- kmymoney/mymoney/mymoneyfile.cpp
+++ kmymoney/mymoney/mymoneyfile.cpp
@@ -4873,15 +4873,11 @@ QMap<QString, QVector<int> > MyMoneyFile::countTransactionsWithSpecificReconcili
     // fill with empty result for all existing accounts
     QList<MyMoneyAccount> list;
     accountList(list);
-    qDebug() << "account entries:" << list.count();
     for (const auto& acc : qAsConst(list)) {
-        qDebug() << "create" << acc.id();
         result[acc.id()] = QVector<int>((int)eMyMoney::Split::State::MaxReconcileState, 0);
     }
-    qDebug() << "result entries:" << result.count();
 
     const auto rows = d->journalModel.rowCount();
-    qDebug() << "processing rows:" << rows;
     QModelIndex idx;
     for (int row = 0; row < rows; ++row) {
         idx = d->journalModel.index(row, 0);
@@ -4892,7 +4888,6 @@ QMap<QString, QVector<int> > MyMoneyFile::countTransactionsWithSpecificReconcili
         case eMyMoney::Split::State::Cleared:
         case eMyMoney::Split::State::Reconciled:
         case eMyMoney::Split::State::Frozen:
-            qDebug() << "Update" << accountId << "with" << static_cast<int>(flag) << "on row" << row;
             result[accountId][(int)flag]++;
             break;
         default:
