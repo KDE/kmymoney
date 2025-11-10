021053635a57962a962e426e2adf225d6b6e5a69
Ralf Habacker <ralf.habacker@freenet.de>
2025-11-10 23:21:09 +0100

    In transaction editor/form add tool tip of the generated reference URL
    This makes it easier to check the generated URL
    
    CCBUG:512098

--- kmymoney/views/newtransactioneditor.cpp
+++ kmymoney/views/newtransactioneditor.cpp
@@ -1149,6 +1149,7 @@ void NewTransactionEditor::Private::updateMemoLink()
         ui->linkLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
         ui->linkLabel->setOpenExternalLinks(true);
         ui->linkLabel->setText(QString("<a href=\"%1\">%2</a>").arg(url.toString(), i18n("Link")));
+        ui->linkLabel->setToolTip(url.toString());
         qDebug() << url;
     } catch (MyMoneyException&) {
         ui->linkLabel->setText("");
