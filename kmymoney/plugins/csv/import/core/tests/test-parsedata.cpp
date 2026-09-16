054075df537cc7ab9343ea293910c1bae93d5550
Thomas Baumgart <thb@net-bembel.de>
2026-09-16 14:08:24 +0200

    Fix decimal symbol detection in CSV parser
    The reported problem most likely occurred when the locale has different settings
    for the decimalSymbol in LC_NUMERIC and LC_MONETARY. Also, the regular
    expression did not do what the adjacent comment suggested it would do.
    
    BUG: 521760
    FIXED-IN: 5.2.3

--- kmymoney/plugins/csv/import/core/tests/test-parsedata.cpp
+++ kmymoney/plugins/csv/import/core/tests/test-parsedata.cpp
@@ -11,6 +11,8 @@
 #include <QString>
 #include <QTest>
 
+#include <mymoneymoney.h>
+
 #include "../csvutil.h"
 
 QTEST_GUILESS_MAIN(ParseDataTest);
@@ -52,12 +54,57 @@ void ParseDataTest::parseSplitString()
 
         auto result = m_parse->parseLine(input);
 
-        QVERIFY(result == expected);
+        QCOMPARE(result, expected);
     }
 }
 
-void ParseDataTest::parse_data()
+void ParseDataTest::testPossiblyReplaceSymbol_data()
 {
+    QTest::addColumn<QString>("inputValue");
+    QTest::addColumn<FieldDelimiter>("fieldDelimiter");
+    QTest::addColumn<DecimalSymbol>("decimalSymbol");
+    QTest::addColumn<QString>("result");
+
+    auto resultString = [](const QString& decimal, const QString& fraction = QString()) {
+        if (fraction.isEmpty()) {
+            return decimal;
+        }
+        return QString(decimal + MyMoneyMoney::decimalSeparator() + fraction);
+    };
+
+    QTest::newRow("comma, dot, dot used parens") << QStringLiteral("(12.34)") << FieldDelimiter::Comma << DecimalSymbol::Dot
+                                                 << resultString(QStringLiteral("-12"), QLatin1String("34"));
+    QTest::newRow("semicolon, comma, comma used negative")
+        << QStringLiteral("-12,34") << FieldDelimiter::Semicolon << DecimalSymbol::Comma << resultString(QStringLiteral("-12"), QLatin1String("34"));
+    QTest::newRow("semicolon, comma, comma used") << QStringLiteral("12,34") << FieldDelimiter::Semicolon << DecimalSymbol::Comma
+                                                  << resultString(QStringLiteral("12"), QLatin1String("34"));
+    QTest::newRow("comma, dot, dot used 1") << QStringLiteral("12.34") << FieldDelimiter::Comma << DecimalSymbol::Dot
+                                            << resultString(QStringLiteral("12"), QLatin1String("34"));
+    QTest::newRow("comma, dot, dot used 2") << QStringLiteral("12.") << FieldDelimiter::Comma << DecimalSymbol::Dot
+                                            << resultString(QStringLiteral("12"), QLatin1String("00"));
+    QTest::newRow("comma, dot, dot used 3") << QStringLiteral("12") << FieldDelimiter::Comma << DecimalSymbol::Dot
+                                            << resultString(QStringLiteral("12"), QLatin1String("00"));
+    QTest::newRow("semicolon, dot, dot used") << QStringLiteral("12.34") << FieldDelimiter::Semicolon << DecimalSymbol::Dot
+                                              << resultString(QStringLiteral("12"), QLatin1String("34"));
+    QTest::newRow("semicolon, dot, comma used") << QStringLiteral("12,34") << FieldDelimiter::Semicolon << DecimalSymbol::Dot
+                                                << resultString(QStringLiteral("1234"));
+    QTest::newRow("semicolon, dot, no decimals 1") << QStringLiteral("12,340") << FieldDelimiter::Semicolon << DecimalSymbol::Dot
+                                                   << resultString(QStringLiteral("12340"), QLatin1String("00"));
+    QTest::newRow("semicolon, dot, no decimals 2") << QStringLiteral("12,340.") << FieldDelimiter::Semicolon << DecimalSymbol::Dot
+                                                   << resultString(QStringLiteral("12340"), QLatin1String("00"));
+}
+
+void ParseDataTest::testPossiblyReplaceSymbol()
+{
+    QFETCH(QString, inputValue);
+    QFETCH(FieldDelimiter, fieldDelimiter);
+    QFETCH(DecimalSymbol, decimalSymbol);
+    QFETCH(QString, result);
+
+    m_parse->setFieldDelimiter(fieldDelimiter);
+    m_parse->setDecimalSymbol(decimalSymbol);
+
+    QCOMPARE(m_parse->possiblyReplaceSymbol(inputValue), result);
 }
 
 void ParseDataTest::cleanupTestCase()
